0490040543925c42023523bf46dd0ade4b7ee756
bbearcookie <aodem@naver.com>
2023-10-20 03:40:01 +0900

    style: main.tsx 불필요한 코드 정리

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import router from '@/routes/router.tsx';
-import App from './App.tsx';
+import router from '@/routes/router';
+import App from './App';
 import './main.css';
+import '@/styles/hello.css';
 
 const queryClient = new QueryClient();
 
