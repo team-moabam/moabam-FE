c33725d0567b36e177b8b8eecae1548ba43a7ca5
bbearcookie <aodem@naver.com>
2023-10-19 23:58:08 +0900

    chore: react-router-dom 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import router from '@/routes/router.tsx';
 import App from './App.tsx';
 import './main.css';
 
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
