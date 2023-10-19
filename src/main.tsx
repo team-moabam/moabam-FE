ccb71ec6764f3eebed2673127f7043e8408b48fa
bbearcookie <aodem@naver.com>
2023-10-19 23:52:46 +0900

    chore: tailwind-css 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
+import './main.css';
 
 const queryClient = new QueryClient();
 
