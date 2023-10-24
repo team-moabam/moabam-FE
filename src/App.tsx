e3c812bf6f5e09b909bce4848537d44d9668ab77
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2023-10-25 00:17:23 +0900

    chore: 배포 CI 스크립트 작성 (#12)
    * chore: 배포 CI 스크립트 작성
    
    * chore: invalidate cloudfront 주석 처리
    
    * style: 라우팅을 main이 아닌 App에서 처리하도록 수정
    
    * chore: 배포 대상 브랜치 main으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import router from '@/routes/router';
+
 const App = () => {
-  return <></>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import router from '@/routes/router';
 import App from './App';
 import './main.css';
 
@@ -13,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
