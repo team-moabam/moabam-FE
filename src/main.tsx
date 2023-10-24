1e3d90fd344ce84888582a0362af3b0deb57dd7e
bbearcookie <aodem@naver.com>
2023-10-24 21:16:37 +0900

    rename: routes, stories 를 core 로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import router from '@/routes/router';
+import router from '@/core/routes/router';
 import App from './App';
 import './main.css';
 
