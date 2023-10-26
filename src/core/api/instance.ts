e3bacde4f0e2fa0994729726423278d1a74c031d
bbearcookie <aodem@naver.com>
2023-10-26 22:10:04 +0900

    remove: axios 인스턴스에 msw 분기 조건 제거

--- src/core/api/instance.ts
+++ src/core/api/instance.ts
@@ -2,9 +2,4 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_BACKEND_API_ENDPOINT;
 
-// const baseURL =
-//   import.meta.env.VITE_MSW_MODE === 'true'
-//     ? ''
-//     : import.meta.env.VITE_BACKEND_API_ENDPOINT;
-
 export const baseInstance = axios.create({ baseURL });
