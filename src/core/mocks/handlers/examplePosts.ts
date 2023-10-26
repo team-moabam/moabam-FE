7a7c211a18c466f1007e3af3b5cf851fbc0d99a1
bbearcookie <aodem@naver.com>
2023-10-26 22:09:04 +0900

    style: MSW 핸들러에 baseURL 적용하는 함수 추가

--- src/core/mocks/handlers/examplePosts.ts
+++ src/core/mocks/handlers/examplePosts.ts
@@ -1,19 +1,20 @@
 // TODO: MSW로 API를 모킹하는 방법을 나타낸 예시 코드에요. 이후에 모킹 함수가 적당히 나온다면 삭제해도 좋은 파일이에요.
 import { http, HttpResponse, delay } from 'msw';
+import { baseURL } from '../baseURL';
 import { ALL_POST } from '../datas/posts';
 
 const examplePostHandlers = [
-  http.get('/example/posts', async () => {
+  http.get(baseURL('/example/posts'), async () => {
     await delay(200);
     return HttpResponse.json(ALL_POST);
   }),
 
-  http.get('/example/posts/:postId', async ({ params }) => {
+  http.get(baseURL('/example/posts/:postId'), async ({ params }) => {
     await delay(200);
     return HttpResponse.json(ALL_POST[Number(params.postId) - 1]);
   }),
 
-  http.post('/example/posts', async ({ request }) => {
+  http.post(baseURL('/example/posts'), async ({ request }) => {
     try {
       const body = await request.json();
       console.log(body);
@@ -25,7 +26,7 @@ const examplePostHandlers = [
     return HttpResponse.json({ id: 101 });
   }),
 
-  http.put('/example/posts/:postId', async ({ request, params }) => {
+  http.put(baseURL('/example/posts/:postId'), async ({ request, params }) => {
     try {
       const body = await request.json();
       console.log(body);
@@ -37,7 +38,7 @@ const examplePostHandlers = [
     return HttpResponse.json({ id: params.postId });
   }),
 
-  http.delete('/example/posts/:postId', async ({ params }) => {
+  http.delete(baseURL('/example/posts/:postId'), async ({ params }) => {
     await delay(200);
     return HttpResponse.json({ id: params.postId });
   })
