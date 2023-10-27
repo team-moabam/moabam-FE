5c6bc5eb40581f9eb7db0ba38e3b48e3df36b5f3
bbearcookie <aodem@naver.com>
2023-10-27 17:53:46 +0900

    feat: 모달 닫는 기능 추가

--- src/shared/BottomSheet/components/BottomSheet.tsx
+++ src/shared/BottomSheet/components/BottomSheet.tsx
@@ -5,11 +5,25 @@ const CONTAINER_ID = 'bottom-sheet';
 
 interface BottomSheetProps {
   isShow: boolean;
+  onClose: VoidFunction;
 }
 
-const BottomSheet = ({ children }: PropsWithChildren<BottomSheetProps>) => {
+const BottomSheet = ({
+  children,
+  isShow,
+  onClose
+}: PropsWithChildren<BottomSheetProps>) => {
+  const handleClose = () => {
+    onClose?.();
+  };
+
   return createPortal(
-    <div className="absolute left-0 top-0 min-h-screen w-full bg-neutral-200">
+    <div
+      className={`absolute left-0 top-0 min-h-screen w-full bg-neutral-200/30 ${
+        !isShow && 'hidden'
+      }`}
+      onClick={handleClose}
+    >
       <div className="absolute bottom-0 w-full bg-red-300">{children}</div>
     </div>,
     document.getElementById(CONTAINER_ID)!
--- src/shared/BottomSheet/hooks/useBottomSheet.tsx
+++ src/shared/BottomSheet/hooks/useBottomSheet.tsx
@@ -1,18 +1,29 @@
-import { useState, useMemo, PropsWithChildren } from 'react';
+import { useState, useCallback, useMemo, PropsWithChildren } from 'react';
 import BottomSheet from '../components/BottomSheet';
 
 const useBottomSheet = () => {
   const [isShow, setIsShow] = useState(false);
 
+  const open = useCallback(() => setIsShow(true), []);
+  const close = useCallback(() => setIsShow(false), []);
+  const toggle = useCallback(() => setIsShow((prev) => !prev), []);
+
   const BottomSheetComponent = useMemo(
     () =>
       ({ children }: PropsWithChildren) => {
-        return <BottomSheet isShow={isShow}>{children}</BottomSheet>;
+        return (
+          <BottomSheet
+            isShow={isShow}
+            onClose={close}
+          >
+            {children}
+          </BottomSheet>
+        );
       },
-    [isShow]
+    [isShow, close]
   );
 
-  return { isShow, setIsShow, BottomSheet: BottomSheetComponent };
+  return { isShow, open, close, toggle, BottomSheet: BottomSheetComponent };
 };
 
 export default useBottomSheet;
