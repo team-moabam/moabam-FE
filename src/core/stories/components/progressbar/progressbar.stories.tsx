61a9b334ebd47e44090f5be6ea74e09339fb0d7c
nsr1349 <nsr1349@naver.com>
2023-10-27 18:04:18 +0900

    fix: 이름 실수

--- src/core/stories/components/progressbar/progressbar.stories.tsx
+++ src/core/stories/components/progressbar/progressbar.stories.tsx
@@ -2,13 +2,13 @@
  * 이 파일처럼, 외부에서 컴포넌트를 가져와서 스토리를 작성할 수도 있습니다.
  */
 import type { Meta, StoryObj } from '@storybook/react';
-import progressbar from './progressbar';
+import ProgressBar from './Progressbar';
 
 const meta = {
   title: 'Components/progressbar',
-  component: progressbar,
+  component: ProgressBar,
   argTypes: { progress: { control: 'range', min: 1, max: 100 } }
-} satisfies Meta<typeof progressbar>;
+} satisfies Meta<typeof ProgressBar>;
 
 export default meta;
 
