494fee026c84c13066acabb3734de7314b32e39c
nsr1349 <nsr1349@naver.com>
2023-10-27 18:14:10 +0900

    feat: Input 작성

--- src/core/stories/components/Input/Input.tsx
+++ src/core/stories/components/Input/Input.tsx
@@ -1,5 +1,26 @@
-const Input = () => {
-  return <></>;
+const Input = ({ ...props }) => {
+  return (
+    <input
+      type="text"
+      className="
+        w-full 
+        rounded-lg
+        border 
+        border-gray-300 
+        p-3 
+        shadow-sm 
+        placeholder:text-gray-400 
+        focus:border-light-point
+        focus:outline-none
+        focus:ring-1
+        focus:ring-light-point 
+        dark:focus:border-dark-point
+        dark:focus:ring-dark-point
+        sm:text-sm
+        "
+      {...props}
+    />
+  );
 };
 
 export default Input;
