df29006fbcdb93e82e81b5712c2c26d9f15276f5
bbearcookie <aodem@naver.com>
2023-10-27 18:17:16 +0900

    feat: BottomSheet 스토리북 작성

--- src/shared/BottomSheet/components/BottomSheet.stories.tsx
+++ src/shared/BottomSheet/components/BottomSheet.stories.tsx
@@ -1,22 +1,35 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import useBottomSheet from '../hooks/useBottomSheet';
 import BottomSheet from './BottomSheet';
 
 const meta = {
-  title: 'Shared/BottomSheet',
-  component: BottomSheet,
-  argTypes: {}
+  title: 'Shared/BottomSheet'
 } satisfies Meta<typeof BottomSheet>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const Page = () => {
+  const { BottomSheet, toggle, close } = useBottomSheet();
+
+  return (
+    <>
+      <button onClick={toggle}>토글</button>
+      <BottomSheet>
+        <h1>하이하이</h1>
+        <div>요건 내부의 내용이에요</div>
+        <div>요건 내부의 내용이에요</div>
+        <div>요건 내부의 내용이에요</div>
+        <div>요건 내부의 내용이에요</div>
+        <button onClick={close}>이거 누르면 BottomSheet를 닫아요</button>
+      </BottomSheet>
+    </>
+  );
+};
+
 export const Default: Story = {
-  args: {},
-  render: (args) => (
-    <BottomSheet {...args}>
-      <h1>하이하이</h1>
-      <div>요건 카드에요</div>
-    </BottomSheet>
-  )
+  render: () => {
+    return <Page />;
+  }
 };
