c890396baefb107f59a17f9fbfae33ea19ac0e06
nsr1349 <nsr1349@naver.com>
2023-10-30 13:43:35 +0900

    feat: className 추가

--- src/core/stories/components/Progressbar/Progressbar.tsx
+++ src/core/stories/components/Progressbar/Progressbar.tsx
@@ -1,15 +1,13 @@
-const ProgressBar = ({ progress = 0, ...props }) => {
-  return (
+const ProgressBar = ({ progress = 0, className = '', ...props }) => (
+  <div
+    className={`h-2.5 w-full overflow-hidden rounded-full bg-gray-300 ${className}`}
+    {...props}
+  >
     <div
-      className="h-2.5 w-full overflow-hidden rounded-full bg-gray-300"
-      {...props}
-    >
-      <div
-        className="h-full bg-light-point dark:bg-dark-point "
-        style={{ width: `${progress}%` }}
-      />
-    </div>
-  );
-};
+      className="h-full bg-light-point dark:bg-dark-point "
+      style={{ width: `${progress}%` }}
+    />
+  </div>
+);
 
 export default ProgressBar;
