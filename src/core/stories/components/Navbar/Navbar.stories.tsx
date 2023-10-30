25bb3fdbc5d9f38229ba2e6bb90036c150448cfa
nsr1349 <nsr1349@naver.com>
2023-10-30 13:46:46 +0900

    feat: 스토리도 같이 수정

--- src/core/stories/components/Navbar/Navbar.stories.tsx
+++ src/core/stories/components/Navbar/Navbar.stories.tsx
@@ -1,6 +1,3 @@
-/**
- * 이 파일처럼, 외부에서 컴포넌트를 가져와서 스토리를 작성할 수도 있습니다.
- */
 import type { Meta, StoryObj } from '@storybook/react';
 import Navbar from './Navbar';
 
@@ -8,7 +5,7 @@ const meta = {
   title: 'Components/Navbar',
   component: Navbar,
   argTypes: {
-    location: { control: 'radio', options: ['/', '/room', '/search', '/my'] }
+    location: {}
   }
 } satisfies Meta<typeof Navbar>;
 
@@ -17,5 +14,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: { location: '/' }
+  args: {}
 };
