c3027ab58113de9fa289fd0b9bcdfca5a33c9097
bbearcookie <aodem@naver.com>
2023-10-30 14:52:13 +0900

    design: 버튼 스타일 @apply로 변경

--- src/shared/Button/styles/buttonVariants.stories.tsx
+++ src/shared/Button/styles/buttonVariants.stories.tsx
@@ -26,57 +26,47 @@ export const Themes: Story = {
     docs: {
       description: {
         story:
-          'buttonVariants() 함수의 첫 번째 인자로 색상 테마를 설정할 수 있습니다.'
+          'className으로 "btn btn-테마" 를 전달하면, 해당 테마의 색상이 적용됩니다.'
       }
     }
   },
   render: () => (
     <div className="flex gap-2">
-      <button className={buttonVariants('danger')}>danger</button>
-      <button className={buttonVariants('success')}>success</button>
-      <button className={buttonVariants('warning')}>warning</button>
-      <button className={buttonVariants('light-point')}>light-point</button>
-      <button className={buttonVariants('dark-point')}>dark-point</button>
+      <button className="btn btn-danger">danger</button>
+      <button className="btn btn-success">success</button>
+      <button className="btn btn-warning">warning</button>
+      <button className="btn btn-light-point">light-point</button>
+      <button className="btn btn-dark-point">dark-point</button>
+      <button
+        className="btn btn-disabled"
+        disabled
+      >
+        disabled
+      </button>
     </div>
   )
 };
 
-export const Options: Story = {
+export const Transitions: Story = {
   parameters: {
     docs: {
       description: {
         story:
-          '모든 버튼은 기본적으로 hover, focus, click, disabled 상태에 따라 스타일이 변경됩니다. 이를 제어하기 위해 두 번째 인자로 객체를 전달할 수 있습니다.'
+          'btn-transition 클래스를 추가하면, hover, active, focus, active 등의 효과가 적용됩니다.'
       }
     }
   },
   render: () => (
     <div className="flex gap-2">
-      <button className={buttonVariants('light-point')}>기본</button>
-      <button className={buttonVariants('light-point', { hover: false })}>
-        hover 제외
+      <button className="btn btn-transition btn-danger">danger</button>
+      <button className="btn btn-transition btn-success">success</button>
+      <button className="btn btn-transition btn-warning">warning</button>
+      <button className="btn btn-transition btn-light-point">
+        light-point
       </button>
-      <button className={buttonVariants('light-point', { focus: false })}>
-        focus 제외
-      </button>
-      <button className={buttonVariants('light-point', { click: false })}>
-        click 제외
-      </button>
-      <button className={buttonVariants('light-point', { click: false })}>
-        click 제외
-      </button>
+      <button className="btn btn-transition btn-dark-point">dark-point</button>
       <button
-        className={buttonVariants('light-point', {
-          hover: false,
-          focus: false,
-          click: false,
-          disabled: false
-        })}
-      >
-        전체 제외
-      </button>
-      <button
-        className={buttonVariants('light-point')}
+        className="btn btn-transition btn-disabled"
         disabled
       >
         disabled
@@ -90,18 +80,18 @@ export const Tags: Story = {
     docs: {
       description: {
         story:
-          '컴포넌트가 아닌 객체로 스타일을 매핑했기 때문에, button이 아닌 다른 태그에도 스타일을 적용할 수 있습니다.'
+          '컴포넌트가 아닌 className으로 스타일을 매핑했기 때문에, button이 아닌 다른 태그에도 스타일을 적용할 수 있습니다.'
       }
     }
   },
   render: () => (
     <div className="flex gap-2">
-      <button className={buttonVariants('light-point')}>button</button>
-      <div className={buttonVariants('light-point')}>div</div>
-      <span className={buttonVariants('light-point')}>span</span>
+      <button className="btn btn-transition btn-light-point">button</button>
+      <div className="btn btn-transition btn-light-point">div</div>
+      <span className="btn btn-transition btn-light-point">span</span>
       <Link
         to="/"
-        className={buttonVariants('success')}
+        className="btn btn-transition btn-light-point"
       >
         Link
       </Link>
@@ -121,19 +111,14 @@ export const Examples: Story = {
   render: () => (
     <div className="flex flex-col items-center justify-center gap-2">
       <div className="w-full">
-        <button
-          className={clsx(buttonVariants('danger'), 'w-full text-center')}
-        >
+        <button className="btn btn-transition btn-danger w-full text-center">
           width 100%
         </button>
       </div>
       <div>
         <button
           disabled
-          className={clsx(
-            buttonVariants('success'),
-            'flex items-center justify-center gap-2'
-          )}
+          className="btn btn-transition btn-success flex items-center justify-center gap-2"
         >
           <div className="h-4 w-4 animate-spin rounded-full border-2 border-solid border-white border-t-transparent" />
           <span>로딩중...</span>
