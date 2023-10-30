a7ebf2808b7c2095ddc08807325603c0a38b3ac8
bbearcookie <aodem@naver.com>
2023-10-30 16:47:27 +0900

    feat: Default 스토리북 작성

--- src/shared/BottomSheet/components/BottomSheet.stories.tsx
+++ src/shared/BottomSheet/components/BottomSheet.stories.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { useBottomSheet, BottomSheet } from '..';
 
@@ -10,28 +10,63 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-// const Page = () => {
-//   const { bottomSheetProps, toggle, close } = useBottomSheet();
-//   const cardRef = useRef(null);
+export const Default: Story = {
+  render: () => {
+    return <DefaultPage />;
+  }
+};
 
-//   return (
-//     <>
-//       <button onClick={toggle}>BottomSheet 열기</button>
-//       <BottomSheet
-//         {...bottomSheetProps}
-//         onClick={() => console.log(cardRef.current)}
-//         ref={cardRef}
-//       >
-//         <h1 className="text-light-point dark:text-red-500">하이하이</h1>
-//         <div>요건 내부의 내용이에요</div>
-//         <div>요건 내부의 내용이에요</div>
-//         <div>요건 내부의 내용이에요</div>
-//         <div>요건 내부의 내용이에요</div>
-//         <button onClick={close}>이거 누르면 BottomSheet를 닫아요</button>
-//       </BottomSheet>
-//     </>
-//   );
-// };
+export const Overflow: Story = {
+  render: () => {
+    return <OverflowPage />;
+  }
+};
+
+const DefaultPage = () => {
+  const { bottomSheetProps, toggle, close } = useBottomSheet();
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  const handleToggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  const cardRef = useRef(null);
+
+  return (
+    <div className={theme === 'dark' ? 'dark' : ''}>
+      <button
+        className="btn btn-transition dark:btn-dark-point"
+        onClick={toggle}
+      >
+        {theme === 'light' ? '화이트' : '다크'} BottomSheet 열기
+      </button>
+      <button
+        className="btn btn-transition btn-success"
+        onClick={handleToggleTheme}
+      >
+        테마 변경
+      </button>
+      <BottomSheet
+        {...bottomSheetProps}
+        theme={theme}
+        onClick={() => console.log(cardRef.current)}
+        ref={cardRef}
+      >
+        <h1 className="text-light-point dark:text-dark-point">하이하이</h1>
+        <div>요건 내부의 내용이에요</div>
+        <div>요건 내부의 내용이에요</div>
+        <div>요건 내부의 내용이에요</div>
+        <div>요건 내부의 내용이에요</div>
+        <button
+          className="btn btn-transition btn-danger"
+          onClick={close}
+        >
+          닫기
+        </button>
+      </BottomSheet>
+    </div>
+  );
+};
 
 const OverflowPage = () => {
   const { bottomSheetProps, toggle, close } = useBottomSheet();
@@ -39,24 +74,29 @@ const OverflowPage = () => {
 
   return (
     <>
-      <button onClick={toggle}>BottomSheet 열기</button>
+      <button
+        className="btn btn-transition btn-light-point"
+        onClick={toggle}
+      >
+        엄청 긴거 열기
+      </button>
       <BottomSheet
         {...bottomSheetProps}
         onClick={() => console.log(cardRef.current)}
+        theme="dark"
         ref={cardRef}
       >
         <h1>내용이 엄청 길면 이렇게 스크롤이 생겨요</h1>
         {Array.from({ length: 50 }).map((_, i) => (
           <div key={i}>{i}</div>
         ))}
-        <button onClick={close}>이거 누르면 BottomSheet를 닫아요</button>
+        <button
+          className="btn btn-transition btn-danger mt-2 w-full"
+          onClick={close}
+        >
+          닫기
+        </button>
       </BottomSheet>
     </>
   );
-};
-
-export const Default: Story = {
-  render: () => {
-    return <OverflowPage />;
-  }
 };
--- src/shared/BottomSheet/components/BottomSheet.tsx
+++ src/shared/BottomSheet/components/BottomSheet.tsx
@@ -1,10 +1,12 @@
 import { useRef, PropsWithChildren, forwardRef } from 'react';
+import clsx from 'clsx';
 import { createPortal } from 'react-dom';
 import { AnimatePresence, motion, HTMLMotionProps } from 'framer-motion';
 
 const CONTAINER_ID = 'bottom-sheet';
 
 export interface BottomSheetProps {
+  theme: 'light' | 'dark'; // TODO: 다크모드에 대한 논의 이후에 수정될 가능성이 높은 코드에요.
   isShow: boolean;
   close: VoidFunction;
 }
@@ -12,7 +14,7 @@ export interface BottomSheetProps {
 const BottomSheet = forwardRef<
   HTMLElement,
   PropsWithChildren<HTMLMotionProps<'section'> & BottomSheetProps>
->(({ children, className, isShow, close, ...props }, ref) => {
+>(({ children, className, theme, isShow, close, ...props }, ref) => {
   const backgroundRef = useRef<HTMLDivElement>(null);
 
   const handleClose: React.MouseEventHandler = (e) => {
@@ -27,9 +29,10 @@ const BottomSheet = forwardRef<
     <AnimatePresence>
       {isShow && (
         <motion.aside
-          className={
-            'dark absolute left-0 top-0 min-h-screen w-full overflow-hidden bg-dark-gray/50'
-          }
+          className={clsx(
+            'absolute left-0 top-0 min-h-screen w-full overflow-hidden bg-dark-gray/50',
+            theme === 'dark' && 'dark'
+          )}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
@@ -37,7 +40,7 @@ const BottomSheet = forwardRef<
           onClick={handleClose}
         >
           <motion.section
-            className={`absolute bottom-0 max-h-full w-full overflow-scroll rounded-t-2xl bg-white p-2 text-black dark:bg-dark-main dark:text-white ${className} shadow-inner drop-shadow-2xl`}
+            className={`absolute bottom-0 max-h-full w-full overflow-auto rounded-t-2xl bg-white p-2 text-black dark:bg-dark-main dark:text-white ${className} shadow-inner drop-shadow-2xl`}
             initial={{ y: '100%' }}
             animate={{ y: '0%' }}
             transition={{ type: 'spring', damping: 20, stiffness: 100 }}
