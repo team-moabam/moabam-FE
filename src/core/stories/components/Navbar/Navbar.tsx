004e76d0fd1a2d0529a07c1a67501a8313cc69f9
nsr1349 <nsr1349@naver.com>
2023-10-30 13:45:55 +0900

    feat: NavLink 추가

--- src/core/stories/components/Navbar/Navbar.tsx
+++ src/core/stories/components/Navbar/Navbar.tsx
@@ -1,11 +1,9 @@
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { HiHome } from 'react-icons/hi2';
 import { ImSearch } from 'react-icons/im';
 import { FaUser, FaClipboardList } from 'react-icons/fa6';
 
 const Navbar = () => {
-  const { pathname } = useLocation();
   const userID = '1234'; //상태에 있을 유저 정보 가져올 예정
 
   const NavbarOptions = [
@@ -23,27 +21,25 @@ const Navbar = () => {
     },
     {
       icon: <FaUser />,
-      route: `/my/${userID}`
+      route: `/user/${userID}`
     }
   ];
+
   return (
-    <>
-      {NavbarOptions.find(({ route }) => route === pathname) && (
-        <nav className="fixed bottom-0 flex h-16 w-full max-w-lg rounded-t-lg bg-light-sub text-2xl text-black dark:bg-dark-sub dark:text-white">
-          {NavbarOptions.map(({ icon, route }) => (
-            <Link
-              className={`grid h-full grow place-items-center transition-all hover:text-light-point dark:hover:text-dark-point ${
-                pathname === route && 'text-light-point dark:text-dark-point'
-              }`}
-              to={route}
-              key={route}
-            >
-              {icon}
-            </Link>
-          ))}
-        </nav>
-      )}
-    </>
+    <nav className="fixed bottom-0 mt-12 flex h-16 w-full max-w-md rounded-t-lg bg-light-sub text-2xl text-black dark:bg-dark-sub dark:text-white">
+      {NavbarOptions.map(({ icon, route }) => (
+        <NavLink
+          to={route}
+          key={route}
+          className={({ isActive }) =>
+            'grid h-full grow place-items-center transition-all hover:text-light-point dark:hover:text-dark-point ' +
+            (isActive && 'text-light-point dark:text-dark-point')
+          }
+        >
+          {icon}
+        </NavLink>
+      ))}
+    </nav>
   );
 };
 
