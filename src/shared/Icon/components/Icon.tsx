56240bce99b1756fc33dac5a7d05849f3f59b2c9
bbearcookie <aodem@naver.com>
2023-10-30 22:13:38 +0900

    feat: Icon 컴포넌트에 size prop 추가

--- src/shared/Icon/components/Icon.tsx
+++ src/shared/Icon/components/Icon.tsx
@@ -1,14 +1,36 @@
 import { IconBaseProps } from 'react-icons';
 import { iconMap } from '../constants/icons';
 
-interface IconProps extends IconBaseProps {
+const sizeMap = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  md: 'text-md',
+  lg: 'text-lg',
+  xl: 'text-xl',
+  '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+  '4xl': 'text-4xl',
+  '5xl': 'text-5xl',
+  '6xl': 'text-6xl',
+  '7xl': 'text-7xl',
+  '8xl': 'text-8xl',
+  '9xl': 'text-9xl'
+};
+
+interface IconProps extends Omit<IconBaseProps, 'size'> {
   icon: keyof typeof iconMap;
+  size?: keyof typeof sizeMap;
 }
 
-const Icon = ({ icon, ...props }: IconProps) => {
+const Icon = ({ icon, size = 'md', className, ...props }: IconProps) => {
   const SelectedIcon = iconMap[icon];
 
-  return <SelectedIcon {...props} />;
+  return (
+    <SelectedIcon
+      className={`${sizeMap[size]} ${className}`}
+      {...props}
+    />
+  );
 };
 
 export default Icon;
