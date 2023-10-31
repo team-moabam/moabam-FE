23860b5205ada7b8483c8ae82f95b92047d97a1e
Sejin Cha <chasj0326@naver.com>
2023-10-31 15:25:07 +0900

    feat: 페이지 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { RouterProvider } from 'react-router-dom';
-import router from '@/core/routes/router';
+import { Link, Outlet } from 'react-router-dom';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <div className="absolute inset-x-0 m-auto flex h-screen w-full max-w-md flex-col border bg-slate-100">
+      <div className="h-full overflow-auto">
+        <Outlet />
+      </div>
+
+      {/* TODO: 임시 네브바, 공통 컴포넌트로 교체해야 함 */}
+      <div className="flex h-16 justify-between bg-slate-300 p-2 text-2xl">
+        <h1>Hello</h1>
+        <Link to="about">about</Link>
+        <Link to="tailwind">Tailwind</Link>
+      </div>
+    </div>
+  );
 };
 
 export default App;
