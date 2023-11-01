b176d3793e4ba36cf00be47a5d73157da6c60f38
Sejin Cha <chasj0326@naver.com>
2023-11-02 01:18:38 +0900

    feat: useRouteData 커스텀훅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Link, Outlet } from 'react-router-dom';
 import routes from './core/routes/routes';
+import useRouteData from './core/hooks/useRouteData';
 
 const App = () => {
+  const { navBarRequired } = useRouteData();
   return (
     /* TODO: 가장 상위 div 에서 다크모드 부여해야 함 */
     <div className="absolute inset-x-0 m-auto flex h-screen w-full max-w-md flex-col">
@@ -9,12 +11,14 @@ const App = () => {
         <Outlet />
       </div>
 
-      {/* TODO: 임시 네브바, 공통 컴포넌트로 교체해야 함 */}
-      <div className="flex h-16 justify-between bg-slate-300 p-2 text-2xl">
-        <Link to={routes.home.path}>home</Link>
-        <Link to={routes.routines.path}>routines</Link>
-        <Link to={routes.mybird.path}>mybird</Link>
-      </div>
+      {navBarRequired && (
+        // TODO: 임시 네브바, 공통 컴포넌트로 교체해야 함
+        <div className="flex h-16 justify-between bg-slate-300 p-2 text-2xl">
+          <Link to={routes.home.path}>home</Link>
+          <Link to={routes.routines.path}>routines</Link>
+          <Link to={routes.mybird.path}>mybird</Link>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/core/hooks/useRouteData.ts
@@ -0,0 +1,48 @@
+import { useLocation, useParams } from 'react-router-dom';
+import routeMap from '../routes/routes';
+
+const isParameterSegment = (segment: string) => segment[0] === ':';
+
+const isMatchPath = (path: string, target: string) => {
+  if (path === target) return true;
+
+  const pathSegments = path.split('/');
+  const targetSegments = target.split('/');
+  if (pathSegments.length !== targetSegments.length) return false;
+
+  return pathSegments.every(
+    (pathSegment, index) =>
+      pathSegment === targetSegments[index] || isParameterSegment(pathSegment)
+  );
+};
+
+const parseQueryString = (queryString: string) => {
+  if (!queryString) return {};
+  return queryString
+    .substring(1)
+    .split('&')
+    .reduce((queryObj, queryItem) => {
+      const [key, value] = queryItem.split('=');
+      return { ...queryObj, [key]: value };
+    }, {});
+};
+
+const useRouteData = () => {
+  const routes = Object.values(routeMap);
+  const params = useParams();
+  const { pathname, search } = useLocation();
+
+  for (const route of routes) {
+    if (isMatchPath(`/${route.path}`, pathname)) {
+      return {
+        ...route,
+        params,
+        search: parseQueryString(search)
+      };
+    }
+  }
+
+  return { ...routeMap.notFound, params: {}, search: {} };
+};
+
+export default useRouteData;
