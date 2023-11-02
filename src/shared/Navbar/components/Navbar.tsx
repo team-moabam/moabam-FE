3cdef08fe4c914aadf3769ce96039a4bac81daa0
nsr1349 <nsr1349@naver.com>
2023-11-02 18:04:30 +0900

    feat: 네브바 빛날 라우트 옵션 배열로 저장, currentPath props 추가

--- src/shared/Navbar/components/Navbar.tsx
+++ src/shared/Navbar/components/Navbar.tsx
@@ -1,42 +1,50 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { HiHome } from 'react-icons/hi2';
 import { ImSearch } from 'react-icons/im';
 import { BiSolidDoorOpen } from 'react-icons/bi';
 import { FaUser } from 'react-icons/fa6';
 
-const Navbar = () => {
+interface NavbarProps {
+  currentPath: string;
+}
+
+const Navbar = ({ currentPath = '' }: NavbarProps) => {
   const NavbarOptions = [
     {
       icon: <HiHome />,
-      route: '/'
+      route: '/',
+      activeRoutes: ['/']
     },
     {
       icon: <BiSolidDoorOpen />,
-      route: '/room/:roomID'
+      route: '/room',
+      activeRoutes: ['/room', '/room/:roomId']
     },
     {
       icon: <ImSearch />,
-      route: '/search'
+      route: '/search',
+      activeRoutes: ['/search']
     },
     {
       icon: <FaUser />,
-      route: `/user/`
+      route: `/user/`,
+      activeRoutes: ['/user/', '/user/:userId']
     }
   ];
 
   return (
     <nav className="flex h-16 w-full rounded-t-lg bg-light-sub text-2xl text-black dark:bg-dark-sub dark:text-white">
-      {NavbarOptions.map(({ icon, route }) => (
-        <NavLink
+      {NavbarOptions.map(({ icon, route, activeRoutes }) => (
+        <Link
           to={route}
           key={route}
-          className={({ isActive }) =>
-            'grid h-full grow place-items-center transition-all hover:text-light-point dark:hover:text-dark-point ' +
-            (isActive && 'text-light-point dark:text-dark-point')
-          }
+          className={`grid h-full grow place-items-center transition-all hover:text-light-point dark:hover:text-dark-point ${
+            activeRoutes.some((Route) => Route === currentPath) &&
+            'text-light-point dark:text-dark-point'
+          } `}
         >
           {icon}
-        </NavLink>
+        </Link>
       ))}
     </nav>
   );
