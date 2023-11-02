06c695cdd8172f73ccb976b95824e58fb9814cd6
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2023-11-02 16:08:35 +0900

    feat: progressbar component (#40)
    * feat: progressbar 일단 생성
    
    * feat: progressbar 및 스토리  완성
    
    * fix: 이름 실수
    
    * feat: 이름잘못 썼는데 적용 자꾸 안되니까 삭제
    
    * feat: 다시 올림
    
    * feat: className 추가
    
    * feat: 쉐어로 옮김
    
    * feat: 상훈님 피드백 적용
    
    ---------
    
    Co-authored-by: Sejin Cha <62418379+chasj0326@users.noreply.github.com>

---
+++ src/shared/ProgressBar/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+interface ProgressBarProps extends React.HTMLAttributes<HTMLDivElement> {
+  progress: number;
+}
+
+const ProgressBar = ({
+  progress = 0,
+  className = '',
+  ...props
+}: ProgressBarProps) => (
+  <div
+    className={`h-2.5 w-full overflow-hidden rounded-full bg-gray-300 ${className}`}
+    {...props}
+  >
+    <div
+      className="h-full bg-light-point dark:bg-dark-point "
+      style={{ width: `${progress}%` }}
+    />
+  </div>
+);
+
+export default ProgressBar;
