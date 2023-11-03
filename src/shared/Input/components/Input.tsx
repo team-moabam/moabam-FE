59e87082efe3f806b49efb3c80afc5544876cfc5
bbearcookie <aodem@naver.com>
2023-11-04 02:50:55 +0900

    refactor: Input에 forwardRef 작업

--- src/shared/Input/components/Input.tsx
+++ src/shared/Input/components/Input.tsx
@@ -1,30 +1,39 @@
+import { forwardRef } from 'react';
+
 export interface InputProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
   size?: 'sm' | 'base' | 'lg';
 }
 
-const Input = ({ size = 'base', className = '', ...props }: InputProps) => (
-  <input
-    className={`
-      w-full
-      rounded-lg 
-      border 
-      border-gray-300 
-      p-3 
-      shadow-sm
-      placeholder:text-gray-400
-      focus:border-light-point
-      focus:outline-none
-      focus:ring-1 
-      focus:ring-light-point
-      dark:focus:border-dark-point
-      dark:focus:ring-dark-point 
-      ${className} 
-      ${sizeVariants[size]}
-      `}
-    {...props}
-  />
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ size = 'base', className, ...props }, ref) => {
+    return (
+      <input
+        className={`
+          w-full
+          rounded-lg
+          border
+          border-gray-300
+          p-3
+          shadow-sm
+          placeholder:text-gray-400
+          focus:border-light-point
+          focus:outline-none
+          focus:ring-1
+          focus:ring-light-point
+          dark:focus:border-dark-point
+          dark:focus:ring-dark-point
+          ${className}
+          ${sizeVariants[size]}
+          `}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
 );
+
+Input.displayName = 'Input';
 
 export default Input;
 
