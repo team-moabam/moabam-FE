55f19ab6e85b121a2b3f68c5c831b71e454ab592
bbearcookie <aodem@naver.com>
2023-11-03 15:26:37 +0900

    style: FunnelHook 타입 작성

--- src/shared/Funnel/hooks/useFunnel.tsx
+++ src/shared/Funnel/hooks/useFunnel.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react';
 import { StepNames } from '../types/funnel';
 
+export interface FunnelHook {
+  current: string;
+  hasPrev: boolean;
+  hasNext: boolean;
+  toPrev: () => void;
+  toNext: () => void;
+}
+
 const useFunnel = <T extends StepNames>(
   steps: T,
   initialStep: T[number] = steps[0]
-) => {
+): FunnelHook => {
   const [current, setCurrent] = useState<T[number]>(initialStep);
   const currentIdx = steps.indexOf(current);
 
