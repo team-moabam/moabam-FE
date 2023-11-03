1a46202c5412625f59e63b39fa510321c5314eb4
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2023-11-03 14:51:20 +0900

    feat: BottomSheet 컴포넌트 작성 (#35)
    * chore: framer-motion 설치
    
    * feat: BottomSheet 레이아웃 작성
    
    * refactor: useBottomSheet에서 컴포넌트 반환하도록 작업
    
    * feat: 모달 닫는 기능 추가
    
    * feat: 모달 컨텐츠 내부 클릭시 닫히는 현상 방지
    
    * feat: BottomSheet 스토리북 작성
    
    * chore: classNames 라이브러리 추가
    
    * chore: classNames 제거 후 clsx 로 대체 설치
    
    * refactor: BottomSheet 컨텐츠 스타일 외부에 위임
    
    * feat: BottomSheet 열고 닫을 때 투명도 효과
    
    * chore: clsx 패키지 삭제
    
    * feat: 모달 컨텐츠 위아래 무빙 애니메이션 효과 추가
    
    * feat: BottomSheet의 하단 카드 부분의 props 개방
    
    * feat: forwardRef로 ref 개방
    
    * feat: BottomSheet 디자인 및 오버플로우 대응
    
    * feat: 엄청 긴 바텀시트 스토리북 작성
    
    * chore: npm install
    
    * feat: Default 스토리북 작성
    
    * feat: Custom 스토리북 작성
    
    * style: hook 반환 값에 as const 추가
    
    * feat: ThemeContext의 PR 내용 가져옴
    
    * feat: #58 PR로부터의 변동사항 반영

---
+++ src/shared/BottomSheet/components/BottomSheet.tsx
@@ -0,0 +1,65 @@
+import { useRef, PropsWithChildren, forwardRef } from 'react';
+import clsx from 'clsx';
+import { createPortal } from 'react-dom';
+import { AnimatePresence, motion, HTMLMotionProps } from 'framer-motion';
+import useTheme from '@/core/hooks/useTheme';
+import { PORTALS } from '@/core/constants/portals';
+
+export interface BottomSheetProps {
+  isShow: boolean;
+  close: VoidFunction;
+}
+
+const BottomSheet = forwardRef<
+  HTMLElement,
+  PropsWithChildren<HTMLMotionProps<'section'> & BottomSheetProps>
+>(({ className, isShow, close, children, ...props }, ref) => {
+  const backgroundRef = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
+
+  const handleClose: React.MouseEventHandler = (e) => {
+    if (e.target !== backgroundRef.current) {
+      return;
+    }
+
+    close && close();
+  };
+
+  return createPortal(
+    <AnimatePresence>
+      {isShow && (
+        <motion.aside
+          className={clsx(
+            'absolute left-0 top-0 min-h-screen w-full overflow-hidden bg-dark-gray/50',
+            theme === 'dark' && 'dark'
+          )}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          ref={backgroundRef}
+          onClick={handleClose}
+        >
+          <motion.section
+            className={clsx(
+              'absolute bottom-0 max-h-full w-full overflow-auto rounded-t-2xl bg-white p-2 text-black shadow-inner drop-shadow-2xl dark:bg-dark-main dark:text-white',
+              className
+            )}
+            initial={{ y: '100%' }}
+            animate={{ y: '0%' }}
+            transition={{ type: 'spring', damping: 20, stiffness: 100 }}
+            exit={{ y: '100%' }}
+            ref={ref}
+            {...props}
+          >
+            {children}
+          </motion.section>
+        </motion.aside>
+      )}
+    </AnimatePresence>,
+    document.getElementById(PORTALS.bottomSheet)!
+  );
+});
+
+BottomSheet.displayName = 'BottomSheet';
+
+export default BottomSheet;
