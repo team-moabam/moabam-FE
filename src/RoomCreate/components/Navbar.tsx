d938445133235d1644d1b19dda225d2a596c233d
bbearcookie <aodem@naver.com>
2023-11-03 15:43:38 +0900

    design: 네비바 작아지는 문제 수정

--- src/RoomCreate/components/Navbar.tsx
+++ src/RoomCreate/components/Navbar.tsx
@@ -2,15 +2,15 @@ import { FunnelHook } from '@/shared/Funnel/hooks/useFunnel';
 
 const Navbar = ({ hasNext, hasPrev, toNext, toPrev }: FunnelHook) => {
   return (
-    <nav className="grid h-16 grid-cols-2 text-2xl">
+    <nav className="grid grid-cols-2 text-2xl">
       <button
-        className="col-start-1 rounded-es-xl bg-white text-dark-gray transition-all hover:bg-slate-100"
+        className="col-start-1 h-16 rounded-es-xl bg-white text-dark-gray transition-all hover:bg-slate-100"
         onClick={toPrev}
       >
         이전
       </button>
       <button
-        className="col-start-2 rounded-ee-xl bg-light-point text-white transition-all hover:bg-light-point-hover"
+        className="col-start-2 h-16 rounded-ee-xl bg-light-point text-white transition-all hover:bg-light-point-hover"
         onClick={toNext}
       >
         다음
