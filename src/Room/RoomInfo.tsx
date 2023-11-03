a14500d6595a542234eb9b01811e83edfe54fd2e
Park Nayeon <within197@naver.com>
2023-11-03 16:23:54 +0900

    feat: RoomInfo UI 추가

---
+++ src/Room/RoomInfo.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ProgressBar } from '@/shared/ProgressBar';
+ProgressBar;
+
+const RoomInfo = () => {
+  return (
+    <div className="relative h-[16.94rem]">
+      <div className="absolute inset-x-0 bottom-0">
+        <div className="mb-[0.5rem] flex items-end justify-between bg-inherit px-[0.87rem]">
+          <span className="block h-[1.93rem] w-[4.62rem] rounded-[6.25rem] bg-light-point py-[0.16rem] text-center text-light-main dark:bg-dark-point">
+            Lv{3}
+          </span>
+          <span className="text-darkGray">
+            {6}/{10}
+          </span>
+        </div>
+        <ProgressBar progress={40}></ProgressBar>
+      </div>
+    </div>
+  );
+};
+
+export default RoomInfo;
