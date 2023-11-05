f27c4661850a16ddbf4a859a48552cc26d5c52c5
bbearcookie <aodem@naver.com>
2023-11-06 01:53:34 +0900

    feat: axios instance 및 interceptor 작업

--- src/core/api/instance.ts
+++ src/core/api/instance.ts
@@ -1,5 +1,24 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const baseURL = import.meta.env.VITE_BACKEND_API_ENDPOINT;
 
 export const baseInstance = axios.create({ baseURL });
+export const formDataInstance = axios.create({ baseURL });
+
+const onSucessResponse = (response: AxiosResponse) => {
+  const { data, status } = response;
+
+  return { statusCode: status, ...data };
+};
+
+const onErrorRequest = (error: unknown) => Promise.reject(error);
+const onErrorResponse = (error: unknown) => Promise.reject(error);
+
+baseInstance.interceptors.response.use(onSucessResponse, onErrorResponse);
+
+formDataInstance.interceptors.request.use((config) => {
+  config.headers['Content-Type'] = 'multipart/form-data';
+  return config;
+}, onErrorRequest);
+
+formDataInstance.interceptors.response.use(onSucessResponse, onErrorResponse);
