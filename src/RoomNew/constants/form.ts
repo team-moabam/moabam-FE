e89ed7f52156d1a10fdd9c219ac43e81be131a0f
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2023-11-05 23:26:45 +0900

    feat: RoomNew 페이지의 스타일, 유효성 스키마 등 상수 정리 (#73)

---
+++ src/RoomNew/constants/form.ts
@@ -0,0 +1,60 @@
+import z from 'zod';
+import {
+  ROOM_TYPES,
+  TIME_RANGE,
+  ROUTINE_NAME,
+  ROOM_NAME,
+  USER_COUNT,
+  PASSWORD
+} from './literals';
+
+const MESSAGE = {
+  ROUTINE_NAME: `루틴 내용은 ${ROUTINE_NAME.min}글자에서 ${ROUTINE_NAME.max}글자 사이여야 해요.`,
+  ROOM_NAME: `방 제목은 ${ROOM_NAME.min}글자에서 ${ROOM_NAME.max}글자 사이여야 해요.`,
+  USER_COUNT: `인원을 올바르게 선택해주세요.`,
+  PASSWORD: `비밀번호는 ${PASSWORD.min}자리에서 ${PASSWORD.max}자리의 숫자여야 해요.}`,
+  ONLY_NUMERIC_PASSWORD: `비밀번호는 숫자로만 입력해주세요.`
+};
+
+export const formSchema = z.object({
+  type: z.enum(ROOM_TYPES),
+  certifyTime: z.number(),
+  routines: z.array(
+    z.object({
+      value: z
+        .string()
+        .trim()
+        .min(ROUTINE_NAME.min, MESSAGE.ROUTINE_NAME)
+        .max(ROUTINE_NAME.max, MESSAGE.ROUTINE_NAME)
+    })
+  ),
+  title: z
+    .string()
+    .trim()
+    .min(ROOM_NAME.min, MESSAGE.ROOM_NAME)
+    .max(ROOM_NAME.max, MESSAGE.ROOM_NAME),
+  userCount: z
+    .number()
+    .gte(USER_COUNT.min, MESSAGE.USER_COUNT)
+    .lte(USER_COUNT.max, MESSAGE.USER_COUNT),
+  password: z.literal('').or(
+    z
+      .string()
+      .min(PASSWORD.min, MESSAGE.PASSWORD)
+      .max(PASSWORD.max, MESSAGE.PASSWORD)
+      .refine((v) => /^\d*$/.test(v), {
+        message: MESSAGE.ONLY_NUMERIC_PASSWORD
+      })
+  )
+});
+
+export const defaultValues: Inputs = {
+  type: 'morning',
+  certifyTime: TIME_RANGE['morning'][0],
+  routines: [{ value: '' }],
+  userCount: 5,
+  title: '',
+  password: ''
+};
+
+export type Inputs = z.infer<typeof formSchema>;
--- src/RoomNew/constants/styles.ts
+++ src/RoomNew/constants/styles.ts
@@ -1,2 +1,8 @@
+// 각 스텝 별 h1 요소에 적용할 스타일
 export const headingStyle = 'text-3xl leading-[3rem]';
+
+// 회색으로 간결한 설명을 적을 때 적용할 스타일
 export const descriptionStyle = 'pt-2 text-base text-dark-gray';
+
+// 에러 메시지를 표시할 때 적용할 스타일
+export const errorStyle = 'ml-2 mt-4 text-red-500 text-sm';
