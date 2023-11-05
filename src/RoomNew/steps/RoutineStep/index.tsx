5d303cb3c58f43df3169312edc541cfbe53ef898
bbearcookie <aodem@naver.com>
2023-11-05 18:52:47 +0900

    feat: RoutineStep 작성

---
+++ src/RoomNew/steps/RoutineStep/index.tsx
@@ -0,0 +1,15 @@
+import RoutineSection from './RoutineSection';
+import NameSection from './NameSection';
+import MemberSection from './MemberSection';
+
+const RoutineStep = () => {
+  return (
+    <>
+      <RoutineSection />
+      <NameSection />
+      <MemberSection />
+    </>
+  );
+};
+
+export default RoutineStep;
