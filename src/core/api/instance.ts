ac1c1bc8d02a907ea5d6568f25e2fca6caa77fe8
bbearcookie <aodem@naver.com>
2023-11-06 03:48:21 +0900

    refactor: multipart를 axios.create의 옵션으로 부여

--- src/core/api/instance.ts
+++ src/core/api/instance.ts
@@ -7,7 +7,10 @@ const baseURL = import.meta.env.VITE_BACKEND_API_ENDPOINT;
 export const baseInstance: CustomAxiosInstance = axios.create({ baseURL });
 
 // multipart/form-data 를 위한 axios 인스턴스
-export const formDataInstance: CustomAxiosInstance = axios.create({ baseURL });
+export const formDataInstance: CustomAxiosInstance = axios.create({
+  baseURL,
+  headers: { 'Content-Type': 'multipart/form-data' }
+});
 
 const onSucessResponse = (res: AxiosResponse) => {
   const { data, status } = res;
@@ -15,14 +18,7 @@ const onSucessResponse = (res: AxiosResponse) => {
   return { ...data, statusCode: status };
 };
 
-const onErrorRequest = (error: any) => Promise.reject(error);
 const onErrorResponse = (error: any) => Promise.reject(error);
 
 baseInstance.interceptors.response.use(onSucessResponse, onErrorResponse);
-
-formDataInstance.interceptors.request.use((config) => {
-  config.headers['Content-Type'] = 'multipart/form-data';
-  return config;
-}, onErrorRequest);
-
 formDataInstance.interceptors.response.use(onSucessResponse, onErrorResponse);
