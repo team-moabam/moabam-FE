ba062c52ca55d79e8ef33cd53013bbd3e79bb00d
Sejin Cha <chasj0326@naver.com>
2023-11-06 18:41:25 +0900

    feat: AccordionGroup 에 containerStyle 추가

--- src/shared/Accordion/components/Accordion.tsx
+++ src/shared/Accordion/components/Accordion.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AccordionContext } from '../hooks/useAccordion';
+import useAccordionGroup from '../hooks/useAccordionGroup';
 
 interface AccordionProps {
   className?: string;
@@ -9,9 +10,14 @@ interface AccordionProps {
 const Accordion = ({ children, className = '' }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => setIsOpen((prev) => !prev);
+  const { containerStyle } = useAccordionGroup();
 
   return (
-    <div className={'w-full overflow-hidden p-1 ' + className}>
+    <div
+      className={
+        'w-full overflow-hidden p-1 ' + `${containerStyle} ` + className
+      }
+    >
       <AccordionContext.Provider value={{ isOpen, toggleOpen }}>
         {children}
       </AccordionContext.Provider>
