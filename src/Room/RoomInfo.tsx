ff5c876e5cb9dc0ab78c42a81dbb1e6b8adb4bd3
Park Nayeon <within197@naver.com>
2023-11-06 12:02:27 +0900

    feat: Room Info 추가

--- src/Room/RoomInfo.tsx
+++ src/Room/RoomInfo.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
 import { ProgressBar } from '@/shared/ProgressBar';
-ProgressBar;
 
 const RoomInfo = () => {
   return (
-    <div className="relative h-[16.94rem]">
+    <div className="relative h-[20.56rem]">
       <div className="absolute inset-x-0 bottom-0">
         <div className="mb-[0.5rem] flex items-end justify-between bg-inherit px-[0.87rem]">
-          <span className="block h-[1.93rem] w-[4.62rem] rounded-[6.25rem] bg-light-point py-[0.16rem] text-center text-light-main dark:bg-dark-point">
+          <span className="block h-[1.93rem] w-[4.62rem] rounded-[6.25rem] bg-light-point py-[0.16rem] text-center font-IMHyemin-bold text-light-main dark:bg-dark-point">
             Lv{3}
           </span>
-          <span className="text-darkGray">
+          <span className="font-IMHyemin-bold text-darkGray">
             {6}/{10}
           </span>
         </div>
