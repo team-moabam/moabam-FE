3d0c3f260566e4e67d2ac3158ca1680f0015db00
bbearcookie <aodem@naver.com>
2023-11-06 18:41:02 +0900

    style: z-index 확장 및 적용

--- src/shared/BottomSheet/components/BottomSheet.tsx
+++ src/shared/BottomSheet/components/BottomSheet.tsx
@@ -30,7 +30,7 @@ const BottomSheet = forwardRef<
       {isShow && (
         <motion.aside
           className={clsx(
-            'app-container bg-dark-gray/50',
+            'app-container z-bottomSheet bg-dark-gray/50',
             theme === 'dark' && 'dark'
           )}
           initial={{ opacity: 0 }}
