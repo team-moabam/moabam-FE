17d465b5605da8f6600195509b44ae5c12f7be6e
Sejin Cha <chasj0326@naver.com>
2023-11-06 18:37:23 +0900

    feat: Accordion 에 w-full, className prop 추가

--- src/shared/Accordion/components/Accordion.tsx
+++ src/shared/Accordion/components/Accordion.tsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import { AccordionContext } from '../hooks/useAccordion';
 
 interface AccordionProps {
+  className?: string;
   children: React.ReactNode;
 }
 
-const Accordion = ({ children }: AccordionProps) => {
+const Accordion = ({ children, className = '' }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => setIsOpen((prev) => !prev);
 
   return (
-    <div className="overflow-hidden p-1">
+    <div className={'w-full overflow-hidden p-1 ' + className}>
       <AccordionContext.Provider value={{ isOpen, toggleOpen }}>
         {children}
       </AccordionContext.Provider>
