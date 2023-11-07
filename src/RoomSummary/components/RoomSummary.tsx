e52b67c9723ccf634aebe3c0e7494042f791983d
Sejin Cha <chasj0326@naver.com>
2023-11-07 16:50:44 +0900

    feat: 다크모드 스타일 수정

--- src/RoomSummary/components/RoomSummary.tsx
+++ src/RoomSummary/components/RoomSummary.tsx
@@ -12,13 +12,14 @@ interface RoomSummaryProps {
 const birdByType = {
   // 임시입니다 mockData 아닙니다!
   MORNING: {
-    containerStyle: 'h-12 w-12 rounded-full',
+    containerStyle: 'h-12 w-12 rounded-full overflow-hidden',
     imgStyle: '',
     bird: '오목눈이',
     imgSrc: 'https://i.ibb.co/QkqCq9J/image-107.png'
   },
   NIGHT: {
-    containerStyle: 'relative h-12 w-12 rounded-full bg-[#FFF5E9]',
+    containerStyle:
+      'relative h-12 w-12 rounded-full bg-[#FFF5E9] overflow-hidden',
     imgStyle: 'absolute left-1 top-1',
     bird: '부엉이',
     imgSrc: 'https://i.ibb.co/HCyhrsM/image-90.png'
