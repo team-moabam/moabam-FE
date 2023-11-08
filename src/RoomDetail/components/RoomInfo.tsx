0a58e534718de68fa969c61b0499f74a9b99ae3d
Park Nayeon <within197@naver.com>
2023-11-08 17:43:22 +0900

    feat: memeber rank 컴포넌트 추가

--- src/RoomDetail/components/RoomInfo.tsx
+++ src/RoomDetail/components/RoomInfo.tsx
@@ -1,25 +1,34 @@
+import RoomMemberRank from './RoomMemberRank';
 import { ProgressBar } from '@/shared/ProgressBar';
-import { RoomInfoType } from '@/core/types/Room';
+import { RoomInfo as RoomInfoType } from '@/core/types/Room';
 
 type RoomInfoProps = {
   roomInfoData: Pick<
     RoomInfoType,
-    'level' | 'currentUserCount' | 'maxUserCount'
+    'level' | 'currentUserCount' | 'maxUserCount' | 'todayCertificateRank'
   >;
 };
 
 const RoomInfo = ({ roomInfoData }: RoomInfoProps) => {
-  const { level, currentUserCount, maxUserCount } = roomInfoData;
+  const { level, currentUserCount, maxUserCount, todayCertificateRank } =
+    roomInfoData;
   return (
     <div className="relative h-[20.56rem]">
+      <RoomMemberRank todayCertificateRank={todayCertificateRank} />
       <div className="absolute inset-x-0 bottom-0">
-        <div className="mb-[0.5rem] flex items-end justify-between bg-inherit px-[0.87rem]">
+        <div className="mb-[0.5rem] flex items-end justify-between bg-inherit pl-[0.87rem] pr-[1.75rem]">
           <span className="block h-[1.93rem] w-[4.62rem] rounded-[6.25rem] bg-light-point py-[0.16rem] text-center font-IMHyemin-bold text-light-main dark:bg-dark-point">
             Lv{level}
           </span>
-          <span className="font-IMHyemin-bold text-dark-gray">
-            {currentUserCount}/{maxUserCount}
-          </span>
+          <div className="flex font-IMHyemin-bold text-[1rem] text-light-gray">
+            <span className="font-IMHyemin-bold">{currentUserCount}</span>
+            <span
+              className="flex font-IMHyemin-bold before:mx-[0.4rem] before:block 
+           before:font-IMHyemin-bold before:text-[1rem] before:content-['/']"
+            >
+              {maxUserCount}
+            </span>
+          </div>
         </div>
         <ProgressBar progress={40}></ProgressBar>
       </div>
