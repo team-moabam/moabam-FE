9498e859a330840ce60bc297a2c0d2385745b156
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2023-11-08 17:41:15 +0900

    feat: 방 생성 API 호출 및 버튼 로딩 처리 (#93)
    * feat: POST /rooms 관련 기능 작성
    
    * feat: POST /rooms 모킹 API status 분기 처리
    
    * feat: API 요청 실패시 에러 메시지 설정 로직 작성
    
    * feat: 전송 버튼 로딩 및 disabled 처리
    
    * fix: Tanstack-Query 공통 에러 타입 오류 수정
    
    * refactor: LoadingSpinner 컴포넌트 사용하도록 변경
    
    ---------
    
    Co-authored-by: Sejin Cha <62418379+chasj0326@users.noreply.github.com>

---
+++ src/core/mocks/handlers/rooms.ts
@@ -0,0 +1,37 @@
+import { http, HttpResponse, delay } from 'msw';
+import { baseURL } from '../baseURL';
+
+const roomsHandlers = [
+  http.post(baseURL('/rooms'), async () => {
+    await delay(1000);
+
+    const status: number = 201;
+    let response = {};
+
+    switch (status) {
+      case 201:
+        response = { message: '모킹 룸 생성 완료', roomId: 1 };
+        break;
+      case 400:
+        response = {
+          message: '유효하지 않은 요청입니다.',
+          validation: {
+            title: 'Title Error',
+            password: 'Password Error',
+            type: 'Type Error',
+            routine: 'Routine Error',
+            certifyTime: 'CertifyTime Error',
+            maxUserCount: 'MaxUserCount Error'
+          }
+        };
+        break;
+      case 401:
+        response = { message: '로그인이 필요합니다.' };
+        break;
+    }
+
+    return HttpResponse.json(response, { status });
+  })
+];
+
+export default roomsHandlers;
