29f66d439cb421f06a0a98220e83c145d662377e
nsr1349 <nsr1349@naver.com>
2023-11-10 02:51:21 +0900

    feat: 스크롤 수정

--- src/pages/LogPage.tsx
+++ src/pages/LogPage.tsx
@@ -3,13 +3,14 @@ import LogList from '@/LogList/components/LogList';
 
 const LogPage = () => {
   return (
-    <>
+    <div className="h-full overflow-auto ">
       <Header
         prev="myPage"
         title="방 참여 기록"
+        className="sticky top-0 bg-light-main dark:bg-dark-main"
       />
       <LogList />
-    </>
+    </div>
   );
 };
 
