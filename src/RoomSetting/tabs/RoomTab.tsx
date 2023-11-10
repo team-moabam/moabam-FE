3ef9ddd9e53c146d99acab7bc1199050bcd5b0ee
bbearcookie <aodem@naver.com>
2023-11-10 20:09:50 +0900

    feat: 공지사항 필드 textarea 적용

--- src/RoomSetting/tabs/RoomTab.tsx
+++ src/RoomSetting/tabs/RoomTab.tsx
@@ -5,13 +5,15 @@ import {
   FormProvider
 } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import ReactTextareaAutosize from 'react-textarea-autosize';
+import clsx from 'clsx';
 import { formatHourString } from '@/TimePicker/utils/hour';
 import {
   Inputs,
   defaultValues,
   formSchema
 } from '@/RoomSetting/constants/form';
-import { TIME_RANGE } from '@/RoomForm/constants/literals';
+import { TIME_RANGE, ANNOUNCEMENT } from '@/RoomForm/constants/literals';
 import { MemberInput, RoutineInputs, PasswordInput } from '@/RoomForm';
 import { Input } from '@/shared/Input';
 import { TimePicker } from '@/TimePicker';
@@ -27,8 +29,11 @@ const RoomTab = () => {
     register,
     handleSubmit,
     setValue,
+    watch,
     formState: { errors }
   } = form;
+
+  const watchAnnouncement = watch('announcement');
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
@@ -57,12 +62,23 @@ const RoomTab = () => {
 
         <section className={sectionStyle}>
           <label
-            className={labelStyle}
+            className={clsx(labelStyle, 'flex justify-between')}
             htmlFor="announcement"
           >
-            공지사항
+            <b>공지사항</b>
+            <p className="text-xs text-gray-400">
+              {watchAnnouncement.length} / {ANNOUNCEMENT.max}
+            </p>
           </label>
-          <Input
+          <ReactTextareaAutosize
+            className={clsx(
+              'w-full resize-none p-3 text-sm',
+              'rounded-lg border border-gray-300 shadow-sm placeholder:text-gray-400',
+              'focus:border-light-point focus:outline-none focus:ring-1 focus:ring-light-point',
+              'dark:focus:border-dark-point dark:focus:ring-dark-point'
+            )}
+            minRows={3}
+            maxLength={ANNOUNCEMENT.max}
             id="announcement"
             {...register('announcement')}
           />
