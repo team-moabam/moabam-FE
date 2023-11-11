d6d691c0ecfe03b8a9225f5f8db27c3609618b39
bbearcookie <aodem@naver.com>
2023-11-11 20:29:20 +0900

    feat: 방 수정 PUT 요청 모킹

--- src/core/mocks/handlers/rooms.ts
+++ src/core/mocks/handlers/rooms.ts
@@ -33,6 +33,41 @@ const roomsHandlers = [
 
     return HttpResponse.json(response, { status });
   }),
+
+  http.put(baseURL('/rooms/:roomId'), async () => {
+    await delay(1000);
+
+    const status: number = 200;
+    let response = {};
+
+    switch (status) {
+      case 200:
+        response = { message: '모킹 룸 수정 완료' };
+        break;
+      case 400:
+        response = {
+          message: '필요한 값이 없습니다.',
+          validation: {
+            title: 'Title Error',
+            announcement: 'Announcement Error',
+            routine: 'Routine Error',
+            password: 'Password Error',
+            certifyTime: 'CertifyTime Error',
+            maxUserCount: 'MaxUserCount Error'
+          }
+        };
+        break;
+      case 401:
+        response = { message: '페이지를 찾을 수 없습니다.' };
+        break;
+      case 404:
+        response = { message: '존재하지 않는 방입니다.' };
+        break;
+    }
+
+    return HttpResponse.json(response, { status });
+  }),
+
   http.get(baseURL('/rooms/:roomId'), async () => {
     await delay(1000);
 
@@ -51,7 +86,7 @@ const roomsHandlers = [
         break;
     }
 
-    return HttpResponse.json(response);
+    return HttpResponse.json(response, { status });
   })
 ];
 
