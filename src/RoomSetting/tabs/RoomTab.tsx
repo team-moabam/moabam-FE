4129ba0663ab544d3579c420c728d8d2d4a72cf0
bbearcookie <aodem@naver.com>
2023-11-11 21:24:26 +0900

    feat: 방 수정 API 호출시 로딩 관련 처리

--- src/RoomSetting/tabs/RoomTab.tsx
+++ src/RoomSetting/tabs/RoomTab.tsx
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
 import {
   SubmitHandler,
   SubmitErrorHandler,
@@ -9,12 +9,14 @@ import ReactTextareaAutosize from 'react-textarea-autosize';
 import clsx from 'clsx';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { roomOptions } from '@/core/api/options';
+import roomAPI from '@/core/api/functions/roomAPI';
 import { formatHourString } from '@/TimePicker/utils/hour';
 import { Inputs, formSchema } from '@/RoomSetting/constants/form';
 import { TIME_RANGE, ANNOUNCEMENT } from '@/RoomForm/constants/literals';
 import { UserCount, Routines, Password } from '@/RoomForm';
 import { Input } from '@/shared/Input';
 import { TimePicker } from '@/TimePicker';
+import { LoadingSpinner } from '@/shared/LoadingSpinner';
 
 interface RoomTabProps {
   roomId: number;
@@ -24,6 +26,10 @@ const RoomTab = ({ roomId }: RoomTabProps) => {
   const { data: room } = useSuspenseQuery({
     ...roomOptions.detail(roomId),
     staleTime: Infinity
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: roomAPI.putRoom
   });
 
   const form = useForm<Inputs>({
@@ -43,6 +49,7 @@ const RoomTab = ({ roomId }: RoomTabProps) => {
     register,
     handleSubmit,
     setValue,
+    setError,
     watch,
     formState: { errors }
   } = form;
@@ -50,7 +57,48 @@ const RoomTab = ({ roomId }: RoomTabProps) => {
   const watchAnnouncement = watch('announcement');
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+    mutate(
+      {
+        roomId,
+        title: data.title,
+        announcement: data.announcement,
+        certifyTime: data.certifyTime % 24,
+        routine: data.routines.map((r) => r.value),
+        maxUserCount: data.userCount,
+        password: data.password
+      },
+      {
+        onSuccess: (data) => console.log(data),
+        onError: (error) => {
+          // TODO: 에러 Toast 메시지를 보여줘야 해요.
+          console.log(error.response?.data?.message);
+
+          if (error.response?.data?.validation) {
+            const {
+              title,
+              announcement,
+              routine,
+              password,
+              certifyTime,
+              maxUserCount
+            } = error.response.data.validation;
+
+            setError('title', { message: title });
+            setError('announcement', { message: announcement });
+            setError('routines', { message: routine });
+            setError('password', { message: password });
+            setError('certifyTime', { message: certifyTime });
+            setError('userCount', { message: maxUserCount });
+          }
+
+          if (error.response?.status === 401) {
+            // TODO: 로그인 페이지로 redirect 해야 해요.
+            // TODO: 혹은 전역 MutationCache에 리다이렉션 관련 로직을 작업해야 해요.
+            console.log('TODO: 로그인 페이지로 redirect');
+          }
+        }
+      }
+    );
   };
 
   const onError: SubmitErrorHandler<Inputs> = (errors) => console.error(errors);
@@ -137,8 +185,14 @@ const RoomTab = ({ roomId }: RoomTabProps) => {
           <Password />
         </section>
 
-        <button className="btn btn-light-point mb-24 w-full text-xl">
-          적용
+        <button
+          className={clsx(
+            'btn btn-light-point mb-24 flex h-12 w-full items-center justify-center text-xl',
+            isPending && 'cursor-not-allowed'
+          )}
+          disabled={isPending}
+        >
+          {isPending ? <LoadingSpinner size="3xl" /> : '수정하기'}
         </button>
       </form>
     </FormProvider>
--- src/core/api/functions/roomAPI.ts
+++ src/core/api/functions/roomAPI.ts
@@ -12,6 +12,18 @@ const roomAPI = {
   }) => {
     return await baseInstance.post<{ message: string }>('/rooms', body);
   },
+  putRoom: async (params: {
+    roomId: number;
+    title: string;
+    announcement: string;
+    routine: string[];
+    password: string;
+    certifyTime: number;
+    maxUserCount: number;
+  }) => {
+    const { roomId, ...body } = params;
+    return await baseInstance.put(`/rooms/${roomId}`, body);
+  },
   getRoomDetail: async (roomId: number) => {
     return await baseInstance.get<RoomInfo>(`/rooms/${roomId}`);
   }
