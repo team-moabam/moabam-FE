51dc873d5e8648181dd3cf637a03f1e118ff1b75
Sejin Cha <chasj0326@naver.com>
2023-11-12 15:41:05 +0900

    feat: 시작페이지 배경 스타일 추가

---
+++ src/pages/StartPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Background from '@/StartSlide/components/Background';
+
+const StartPage = () => {
+  return (
+    <>
+      <Background type="night" />
+    </>
+  );
+};
+
+export default StartPage;
