45edead220157208b1c7af9ab080ae49d4e88841
bbearcookie <aodem@naver.com>
2023-11-14 02:19:28 +0900

    feat: 방 삭제 API 호출 완성

--- src/RoomSetting/tabs/RemoveTab.tsx
+++ src/RoomSetting/tabs/RemoveTab.tsx
@@ -1,16 +1,37 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
 import { roomOptions } from '@/core/api/options';
+import roomAPI from '@/core/api/functions/roomAPI';
+import { useMoveRoute } from '@/core/hooks';
 import { Input } from '@/shared/Input';
+import { LoadingSpinner } from '@/shared/LoadingSpinner';
 
 interface RemoveTabProps {
   roomId: string;
 }
 
 const RemoveTab = ({ roomId }: RemoveTabProps) => {
+  const [confirmInput, setConfirmInput] = useState('');
+  const moveTo = useMoveRoute();
+
   const { data: room } = useSuspenseQuery({
     ...roomOptions.detail(roomId),
     staleTime: Infinity
   });
+
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: roomAPI.deleteRoom
+  });
+
+  const handleRemove = () => {
+    mutate(roomId, {
+      onSuccess: () => {
+        moveTo('routines');
+        // TODO: 토스트 메시지로 방을 삭제했음을 알려야 해요.
+      },
+      onError: (err) => console.error(err)
+    });
+  };
 
   if (room.currentUserCount > 1) {
     return (
@@ -25,11 +46,32 @@ const RemoveTab = ({ roomId }: RemoveTabProps) => {
     return (
       <>
         <h1 className={headingStyle}>방을 삭제할까요?</h1>
-        <section className="my-6 flex flex-col gap-2">
-          <label className={descriptionStyle}>방의 이름을 적어주세요</label>
-          <Input size="sm" />
+        <section className="mt-4 flex flex-col gap-2">
+          <label
+            htmlFor="confirm"
+            className={descriptionStyle}
+          >
+            방의 이름 "{room.title}" 을 적어주세요.
+          </label>
+          <Input
+            id="confirm"
+            value={confirmInput}
+            placeholder={room.title}
+            onChange={(e) => setConfirmInput(e.target.value)}
+          />
         </section>
-        <button className="btn btn-danger w-full">방 삭제</button>
+        {error && (
+          <p className="ml-2 mt-2 text-base text-danger">
+            {error.response?.data.message}
+          </p>
+        )}
+        <button
+          className="btn btn-danger mt-8 flex w-full items-start justify-center"
+          onClick={handleRemove}
+          disabled={confirmInput !== room.title}
+        >
+          {isPending ? <LoadingSpinner size="2xl" /> : '방 삭제'}
+        </button>
       </>
     );
   }
