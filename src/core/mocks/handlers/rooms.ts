3c0aac608dd784187c90a07310e8b26f04ead3ee
bbearcookie <aodem@naver.com>
2023-11-14 01:37:43 +0900

    feat: 방 삭제, 방장 위임, 멤버 추방 API 함수 및 모킹 핸들러 작성

--- src/core/mocks/handlers/rooms.ts
+++ src/core/mocks/handlers/rooms.ts
@@ -34,6 +34,27 @@ const roomsHandlers = [
     return HttpResponse.json(response, { status });
   }),
 
+  http.get(baseURL('/rooms/:roomId'), async () => {
+    await delay(1000);
+
+    const status: number = 200;
+    let response = {};
+
+    switch (status) {
+      case 200:
+        response = RoomInfo;
+        break;
+      case 401:
+        response = { message: '존재하지 않는 유저입니다.' };
+        break;
+      case 404:
+        response = { message: '존재하지 않는 방입니다.' };
+        break;
+    }
+
+    return HttpResponse.json(response, { status });
+  }),
+
   http.put(baseURL('/rooms/:roomId'), async () => {
     await delay(1000);
 
@@ -68,7 +89,7 @@ const roomsHandlers = [
     return HttpResponse.json(response, { status });
   }),
 
-  http.get(baseURL('/rooms/:roomId'), async () => {
+  http.delete(baseURL('/rooms/:roomId'), async () => {
     await delay(1000);
 
     const status: number = 200;
@@ -76,13 +97,57 @@ const roomsHandlers = [
 
     switch (status) {
       case 200:
-        response = RoomInfo;
+        response = {};
+        break;
+      case 400:
+        response = {
+          message: '방장은 나가려면 위임 하거나 혼자 남아있어야 합니다.'
+        };
         break;
       case 401:
         response = { message: '존재하지 않는 유저입니다.' };
         break;
-      case 404:
-        response = { message: '존재하지 않는 방입니다.' };
+    }
+
+    return HttpResponse.json(response, { status });
+  }),
+
+  http.delete(baseURL('/rooms/:roomId/members/:memberId'), async () => {
+    await delay(1000);
+
+    const status: number = 200;
+    let response = {};
+
+    switch (status) {
+      case 200:
+        response = {};
+        break;
+      case 400:
+        response = { message: '방장만 추방할 수 있습니다.' };
+        break;
+      case 401:
+        response = { message: '존재하지 않는 유저입니다.' };
+        break;
+    }
+
+    return HttpResponse.json(response, { status });
+  }),
+
+  http.put(baseURL('/rooms/:roomId/members/:memberId/delegation'), async () => {
+    await delay(1000);
+
+    const status: number = 200;
+    let response = {};
+
+    switch (status) {
+      case 200:
+        response = {};
+        break;
+      case 400:
+        response = { message: '방장만 위임할 수 있습니다.' };
+        break;
+      case 401:
+        response = { message: '존재하지 않는 유저입니다.' };
         break;
     }
 
