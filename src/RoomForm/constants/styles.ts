fe349785c60fd7eabd76771f188598d30b58c7b3
bbearcookie <aodem@naver.com>
2023-11-13 17:25:04 +0900

    feat: 플러스 아이콘 버튼 처리 및 다크모드 대응

--- src/RoomForm/constants/styles.ts
+++ src/RoomForm/constants/styles.ts
@@ -1,6 +1,6 @@
 // 플러스(+), 마이너스(-) 버튼에 적용할 스타일
 export const iconButtonStyle =
-  'cursor-pointer text-light-point transition-all hover:text-light-point-hover';
+  'cursor-pointer text-light-point dark:text-dark-point transition-all hover:text-light-point-hover hover:dark:text-dark-point-hover';
 
 // 에러 메시지를 표시할 때 적용할 스타일
 export const errorStyle = 'ml-2 mt-4 text-red-500 text-sm';
