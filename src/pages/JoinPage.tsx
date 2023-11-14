0a3ffcb3772462de30369383ccfed737000f8c09
nsr1349 <nsr1349@naver.com>
2023-11-14 14:25:53 +0900

    feat: 기존 이미지 삭제, 스타일 수정

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -4,17 +4,16 @@ const JoinPage = () => {
       <div className="absolute inset-x-0 top-1/3 flex flex-col items-center">
         <div className="mb-8 aspect-square w-3/5 max-w-[200px]">
           <img
-            src="public\joinBird.jpg"
+            src="public\assets\Omok.png"
             alt=""
             className="w-full  rounded-full"
           />
         </div>
         <div className="flex flex-col items-center text-center">
-          <h1 className="mb-4 text-2xl">안녕하세요!</h1>
-          <h3 className=" text-dark-gray">
-            다른 사람들과 같은 곳을 바라보면서
+          <h1 className="mb-4 text-3xl">안녕하세요!</h1>
+          <h3 className=" text-lg text-dark-gray">
+            다른 사람들과 같은 곳을 바라보면서 <br /> 루틴에 활기를 찾아봐요
           </h3>
-          <h3 className="text-dark-gray">루틴에 활기를 찾아봐요</h3>
         </div>
       </div>
       <div className="absolute inset-x-0 bottom-5 mx-5 overflow-hidden rounded-full">
