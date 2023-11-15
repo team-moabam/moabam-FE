a0df66245599efba5aac3141398fedbeec852854
bbearcookie <aodem@naver.com>
2023-11-15 16:28:34 +0900

    fix: statusCode 파싱 로직 제거

--- src/core/api/types.ts
+++ src/core/api/types.ts
@@ -20,61 +20,56 @@ declare module '@tanstack/react-query' {
   }
 }
 
-// statusCode를 포함한 응답 타입
-export type ResponseWithStatusCode<T = any> = T & { statusCode: number };
-
-// AxiosInstance 인터페이스의 내용을 그대로 확장하여 응답에 ResponseWithStatusCode를 적용한 인터페이스
+// AxiosInstance 인터페이스의 내용을 그대로 확장하고 응답에서 data 만 파싱한 인터페이스
 export interface CustomAxiosInstance extends AxiosInstance {
   interceptors: {
     request: AxiosInterceptorManager<InternalAxiosRequestConfig>;
     response: AxiosInterceptorManager<AxiosResponse<any>>;
   };
   getUri(config?: AxiosRequestConfig): string;
-  request<T = any, R = ResponseWithStatusCode<T>, D = any>(
-    config: AxiosRequestConfig<D>
-  ): Promise<R>;
-  get<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  request<T = any, R = T, D = any>(config: AxiosRequestConfig<D>): Promise<R>;
+  get<T = any, R = T, D = any>(
     url: string,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  delete<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  delete<T = any, R = T, D = any>(
     url: string,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  head<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  head<T = any, R = T, D = any>(
     url: string,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  options<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  options<T = any, R = T, D = any>(
     url: string,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  post<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  post<T = any, R = T, D = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  put<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  put<T = any, R = T, D = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  patch<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  patch<T = any, R = T, D = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  postForm<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  postForm<T = any, R = T, D = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  putForm<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  putForm<T = any, R = T, D = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig<D>
   ): Promise<R>;
-  patchForm<T = any, R = ResponseWithStatusCode<T>, D = any>(
+  patchForm<T = any, R = T, D = any>(
     url: string,
     data?: D,
     config?: AxiosRequestConfig<D>
