3035799f2ec3d0cbc1c4884369013d283637c7be
bbearcookie <aodem@naver.com>
2023-11-17 15:54:02 +0900

    refactor: fallback UI 분리

--- src/shared/QueryErrorBoundary/components/QueryErrorBoundary.tsx
+++ src/shared/QueryErrorBoundary/components/QueryErrorBoundary.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import { ErrorBoundary } from '@suspensive/react';
+import { ErrorBoundary, ErrorBoundaryFallbackProps } from '@suspensive/react';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 
-const QueryErrorBoundary = ({ children }: React.PropsWithChildren) => {
+interface QueryErrorBoundaryProps {
+  fallback:
+    | React.ReactNode
+    | React.FunctionComponent<ErrorBoundaryFallbackProps<Error>>;
+}
+
+const QueryErrorBoundary = ({
+  fallback,
+  children
+}: React.PropsWithChildren<QueryErrorBoundaryProps>) => {
   const { reset } = useQueryErrorResetBoundary();
 
   return (
     <ErrorBoundary
       onReset={reset}
-      fallback={(props) => (
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <img
-            src="/assets/PageError.png"
-            alt="오류"
-          />
-          <h1 className="mt-5 text-3xl font-bold">오류 발생</h1>
-          <p className="text-lg leading-10 text-dark-gray">
-            정보를 불러올 수 없어요.
-          </p>
-          <button
-            className="btn btn-light-point dark:btn-dark-point mt-5 rounded-lg"
-            onClick={props.reset}
-          >
-            다시 불러오기
-          </button>
-        </div>
-      )}
+      fallback={fallback}
     >
       {children}
     </ErrorBoundary>
--- src/shared/QueryErrorBoundary/index.ts
+++ src/shared/QueryErrorBoundary/index.ts
@@ -1,1 +1,2 @@
 export { default as QueryErrorBoundary } from './components/QueryErrorBoundary';
+export { default as NetworkFallback } from './components/NetworkFallback';
