deb24df01de0a9f1bc4cf6ddaf755ca908884052
bbearcookie <aodem@naver.com>
2023-11-17 15:47:57 +0900

    feat: QueryErrorBoundary 화면 작성

--- src/shared/QueryErrorBoundary/components/QueryErrorBoundary.tsx
+++ src/shared/QueryErrorBoundary/components/QueryErrorBoundary.tsx
@@ -9,9 +9,21 @@ const QueryErrorBoundary = ({ children }: React.PropsWithChildren) => {
     <ErrorBoundary
       onReset={reset}
       fallback={(props) => (
-        <div>
-          <div>재시도하세요!!</div>
-          <button onClick={props.reset}>트라이 어게인</button>
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <img
+            src="/assets/PageError.png"
+            alt="오류"
+          />
+          <h1 className="mt-5 text-3xl font-bold">오류 발생</h1>
+          <p className="text-lg leading-10 text-dark-gray">
+            정보를 불러올 수 없어요.
+          </p>
+          <button
+            className="btn btn-light-point dark:btn-dark-point mt-5 rounded-lg"
+            onClick={props.reset}
+          >
+            다시 불러오기
+          </button>
         </div>
       )}
     >
