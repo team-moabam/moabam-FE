9660fe179b43c0be618d7cadabd5c79977670c4b
bbearcookie <aodem@naver.com>
2023-11-17 16:21:08 +0900

    style: QueryErrorBoundary JSDOC 주석 추가

--- src/shared/ErrorBoundary/components/QueryErrorBoundary.tsx
+++ src/shared/ErrorBoundary/components/QueryErrorBoundary.tsx
@@ -8,6 +8,12 @@ interface QueryErrorBoundaryProps {
     | React.FunctionComponent<ErrorBoundaryFallbackProps<Error>>;
 }
 
+/**
+ * QueryErrorBoundary를 래핑한 ErrorBoundary 컴포넌트입니다.
+ * 쿼리 에러가 발생했을 때, 쿼리를 리셋하는 함수를 담고 fallback을 렌더링합니다.
+ * @param fallback - 오류 발생 시 렌더링할 컴포넌트.
+ * @param children - 성공 시 렌더링할 컴포넌트.
+ */
 const QueryErrorBoundary = ({
   fallback,
   children
