c8fe2e42d099aa143c4d82fdc2b6fde8e55b4278
nsr1349 <nsr1349@naver.com>
2023-11-19 02:43:24 +0900

    feat: 헤더 지갑 부문 작성

--- src/MyBird/components/HeaderWallet.tsx
+++ src/MyBird/components/HeaderWallet.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import { useContextSelector } from 'use-context-selector';
+import { BiSolidBugAlt } from 'react-icons/bi';
 import { MyBirdContext } from '../contexts/MyBirdContext';
 
 const HeaderWallet = () => {
   const bugs = useContextSelector(MyBirdContext, (state) => state.bugs);
-  const setBugs = useContextSelector(MyBirdContext, (state) => state.setBugs);
 
   useEffect(() => {
     console.log('HeaderWallet');
@@ -12,19 +12,31 @@ const HeaderWallet = () => {
   });
   return (
     <>
-      <button
-        onClick={() =>
-          setBugs({
-            morningBug: 4,
-            nightBug: 300,
-            goldenBug: 100
-          })
-        }
-      >
-        ddddddd
-      </button>
+      <div className="flex gap-3 rounded-full border-2 border-dark-gray bg-[rgba(1,1,1,0.5)] px-3 py-1">
+        {bugArray.map((bug) => (
+          <div
+            className={`flex items-center gap-2 ${bugColor[bug]}`}
+            key={bug}
+          >
+            <BiSolidBugAlt />
+            <h1>{bugs[bug]}</h1>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
 
 export default HeaderWallet;
+
+const bugArray: ('morningBug' | 'nightBug' | 'goldenBug')[] = [
+  'morningBug',
+  'nightBug',
+  'goldenBug'
+];
+
+const bugColor = {
+  morningBug: 'text-light-point',
+  nightBug: 'text-dark-point',
+  goldenBug: 'text-warning'
+};
