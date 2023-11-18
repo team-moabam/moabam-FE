b6d37dcde057ad62265599a2c65bf70f33ea1e9f
nsr1349 <nsr1349@naver.com>
2023-11-19 04:32:21 +0900

    feat: useDebounce 수정

--- src/core/hooks/useDebounce.ts
+++ src/core/hooks/useDebounce.ts
@@ -1,26 +1,23 @@
 import { useEffect, useRef } from 'react';
 
-function useDebounce(fn: (...args: any[]) => any, delay: number) {
+const useDebounce = <T extends any[]>(
+  fn: (...args: T) => any,
+  delay: number
+) => {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  function debouncedFn(...args: any[]) {
+  useEffect(() => {
+    timeoutRef.current && clearTimeout(timeoutRef.current);
+  }, []);
+
+  return (...args: T) => {
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
     }
     timeoutRef.current = setTimeout(() => {
       fn(...args);
     }, delay);
-  }
-
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-    };
-  }, []);
-
-  return debouncedFn;
-}
+  };
+};
 
 export default useDebounce;
