e44767fde6820e8c77d3d363fe1f4ec1ee36bbe6
nsr1349 <nsr1349@naver.com>
2023-11-20 13:56:58 +0900

    feat: 임시 목데이터 작성

---
+++ src/core/mocks/datas/orderLog.ts
@@ -0,0 +1,73 @@
+interface History {
+  id: number;
+  bugType: 'MORNING_BUG' | 'NIGHT_BUG' | 'GOLDEN_BUG';
+  action: '보상' | '쿠폰' | '충전';
+  amount: number;
+  date: string;
+  paymentInfo?: {
+    id: number;
+    count: number;
+    price: number;
+    discount: number;
+    provider: string;
+  };
+}
+
+export const data: { history: History[] } = {
+  history: [
+    {
+      id: 3,
+      bugType: 'MORNING_BUG',
+      action: '보상',
+      amount: 2,
+      date: '2023.11.01 23:50'
+    },
+    {
+      id: 2,
+      bugType: 'NIGHT_BUG',
+      action: '쿠폰',
+      amount: 5,
+      date: '2023.10.25 23:50'
+    },
+    {
+      id: 1,
+      bugType: 'GOLDEN_BUG',
+      action: '충전',
+      amount: 10,
+      date: '2023.10.19 23:50',
+      paymentInfo: {
+        id: 1,
+        count: 10,
+        price: 3000,
+        discount: 1000,
+        provider: '...'
+      }
+    }
+  ]
+};
+
+interface Bug {
+  color: 'text-light-point' | 'text-dark-point' | 'text-warning';
+  name: string;
+}
+
+export interface Bugs {
+  MORNING_BUG: Bug;
+  NIGHT_BUG: Bug;
+  GOLDEN_BUG: Bug;
+}
+
+export const bugs: Bugs = {
+  MORNING_BUG: {
+    color: 'text-light-point',
+    name: '낮벌레'
+  },
+  NIGHT_BUG: {
+    color: 'text-dark-point',
+    name: '밤벌레'
+  },
+  GOLDEN_BUG: {
+    color: 'text-warning',
+    name: '황금벌레'
+  }
+};
