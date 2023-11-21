2b921747c811ccfe697ace69774c26d0bf7af053
Park Nayeon <within197@naver.com>
2023-11-21 18:31:15 +0900

    chore: member 타입 수정

--- src/core/types/Member.ts
+++ src/core/types/Member.ts
@@ -2,6 +2,7 @@ export type Member = {
   memberId: string;
   nickname: string;
   profileImage: string;
+  isNotificationSent: boolean;
   contributionPoint: number;
   awakeImage: string;
   sleepImage: string;
