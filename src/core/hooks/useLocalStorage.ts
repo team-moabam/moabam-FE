067e5729a88f6923c2d88387938a12cca9704ea0
bbearcookie <aodem@naver.com>
2023-11-21 17:59:25 +0900

    style: useLocalStorage JSDoc 주석 추가

--- src/core/hooks/useLocalStorage.ts
+++ src/core/hooks/useLocalStorage.ts
@@ -1,6 +1,12 @@
 import { useState, useCallback } from 'react';
 import type { Dispatch, SetStateAction } from 'react';
 
+/**
+ * 상태를 로컬 스토리지와 동기화하는 커스텀 훅
+ * @param key 로컬 스토리지에 저장할 키 이름
+ * @param initialValue 상태의 기본 값
+ * @returns [state, setState]
+ */
 const useLocalStorage = <T>(
   key: string,
   initialValue: T
