f89130c3a6250af2e02256a5d6b23aea127c09c8
Park Nayeon <within197@naver.com>
2023-11-21 18:26:12 +0900

    feat: 콕찌르기 msw handler 생성 및 mock 데이터 수정

--- src/core/mocks/handlers/rooms.ts
+++ src/core/mocks/handlers/rooms.ts
@@ -140,6 +140,33 @@ const roomsHandlers = [
     return HttpResponse.json(response, { status });
   }),
 
+  http.get(baseURL('/rooms/:roomId/:memberId'), async () => {
+    await delay(1000);
+
+    const status: number = 200;
+    let response = {};
+
+    switch (status) {
+      case 200:
+        response = {};
+        break;
+      case 400:
+        response = {
+          message: '콕 찌르기는 각 유저마다 인증타임 당 1회 가능합니다.'
+        };
+        break;
+      case 401:
+        response = { message: '로그인 후 이용 가능합니다.' };
+        break;
+      case 404:
+        response = {
+          message: '존재하지 않는 유저입니다.'
+        };
+    }
+
+    return HttpResponse.json({ body: response, status });
+  }),
+
   http.put(baseURL('/rooms/:roomId/members/:memberId/delegation'), async () => {
     await delay(1000);
 
