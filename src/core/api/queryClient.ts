11d0774b2b08a3da274652654814e0190137299b
bbearcookie <aodem@naver.com>
2023-11-22 21:46:16 +0900

    refactor: 로그인 리다이렉션 수정

--- src/core/api/queryClient.ts
+++ src/core/api/queryClient.ts
@@ -2,11 +2,13 @@ import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
 import memberOptions from '@/core/api/options/member';
 import router from '@/core/routes/router';
 import { CustomAxiosError } from '@/core/api/types';
+import { STORAGE_KEYS } from '../constants/storageKeys';
 
 const handleRedirectOnError = (error: CustomAxiosError) => {
   if (error.response?.status === 401) {
     router.navigate('/join');
   }
+  localStorage.clearItem(STORAGE_KEYS.MEMBER_ID);
 };
 
 const queryClient = new QueryClient({
--- src/useCheckAuthRequired.ts
+++ src/useCheckAuthRequired.ts
@@ -1,6 +1,4 @@
 import { useEffect } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import memberOptions from '@/core/api/options/member';
 import { useLocalStorage, useMoveRoute, useRouteData } from '@/core/hooks';
 import { STORAGE_KEYS } from '@/core/constants/storageKeys';
 
@@ -8,7 +6,6 @@ import { STORAGE_KEYS } from '@/core/constants/storageKeys';
  * 로그인이 필요한 페이지에서 로그인을 여부를 체크하고 로그인 페이지로 이동시키는 훅
  */
 const useCheckAuthRequired = () => {
-  const { error } = useQuery({ ...memberOptions.myInfo() });
   const { authRequired } = useRouteData();
   const [memberId, setMemberId] = useLocalStorage<number | null>(
     STORAGE_KEYS.MEMBER_ID,
@@ -16,19 +13,11 @@ const useCheckAuthRequired = () => {
   );
   const moveTo = useMoveRoute();
 
-  // 로컬 스토리지에 memberId가 없다면 로그인 페이지로 이동
   useEffect(() => {
     if (authRequired && !memberId) {
       moveTo('join');
     }
   }, [memberId]);
-
-  // 로그인이 필요한 페이지에서 401 에러가 발생하면 로컬 스토리지 초기화 후 로그인 페이지로 이동
-  useEffect(() => {
-    if (authRequired && error?.response?.status === 401) {
-      setMemberId(null);
-    }
-  }, [authRequired, error]);
 };
 
 export default useCheckAuthRequired;
