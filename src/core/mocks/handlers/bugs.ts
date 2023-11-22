a3033f5181a6bd824b887b9d124dec475b07f247
Sejin Cha <chasj0326@naver.com>
2023-11-22 18:34:04 +0900

    fix: 빌드 에러 수정

--- src/core/mocks/handlers/bugs.ts
+++ src/core/mocks/handlers/bugs.ts
@@ -1,12 +1,11 @@
 import { http, HttpResponse, delay } from 'msw';
 import { baseURL } from '../baseURL';
-import { TODAY_BUGS } from '../datas/todayBugs';
 
 const bugsHandlers = [
   http.get(baseURL('/bugs/today'), async () => {
     await delay(1000);
-    return HttpResponse.json(TODAY_BUGS, { status: 200 });
-  })
+    return HttpResponse.json({}, { status: 200 });
+  }) // TODO: 나중에 bugs 다른 핸들러 머지되면 삭제하는 코드입니다. 충돌날까봐 남겨둡니다..
 ];
 
 export default bugsHandlers;
