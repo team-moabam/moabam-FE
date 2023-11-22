d82804c5f516fd95e64509b59d690ad57413a789
Sejin Cha <62418379+chasj0326@users.noreply.github.com>
2023-11-22 17:43:51 +0900

    feat: 쿠폰 배너 수정 및 이벤트 페이지 완성 (#194)
    * feat: 메인페이지 쿠폰 배너 수정
    
    * docs: Room 이동 로직 메모 추가
    
    * feat: 이벤트 페이지 UI 수정 및 api 연결
    
    * feat: disabledCover 스타일 수정
    
    * feat: 쿠폰 발급 기능 구현
    
    * feat: 스켈레톤 로딩 처리 추가
    
    * feat: 이벤트 페이지 헤더 prev 기능 수정
    
    * refactor: 완료된 주석 삭제
    
    * feat: 쿠폰 API 명세 수정 반영
    
    * feat: query option 코드리뷰 반영

---
+++ src/Promotion/components/DisabledCover.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { clsx } from 'clsx';
+import { Icon } from '@/shared/Icon';
+
+interface DisabledCoverProps {
+  dueType: 'opened' | 'ended';
+  startDiff: number;
+}
+
+const DisabledCover = ({ dueType, startDiff }: DisabledCoverProps) => {
+  return (
+    <div className="absolute inset-0 flex justify-center bg-black bg-opacity-[0.4] text-white">
+      {dueType === 'ended' ? (
+        <span
+          className={clsx(
+            'mt-24 h-fit rounded-full px-3 py-1 font-IMHyemin-bold',
+            'bg-black bg-opacity-[0.3]'
+          )}
+        >
+          종료된 쿠폰입니다
+        </span>
+      ) : (
+        <div
+          className={clsx(
+            'mt-16 h-36 w-36 rounded-full',
+            'border-3 border-white bg-black bg-opacity-[0.4]',
+            'flex flex-col items-center gap-3 pt-6'
+          )}
+        >
+          <Icon
+            icon="TbConfetti"
+            color="white"
+            size="3xl"
+          />
+          <div className="font-IMHyemin-bold text-3xl">D-{startDiff}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DisabledCover;
--- src/Promotion/components/EventCard.tsx
+++ src/Promotion/components/EventCard.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useMutation } from '@tanstack/react-query';
 import { clsx } from 'clsx';
-import useToast from '@/shared/Toast/hooks/useToast';
+import couponAPI from '@/core/api/functions/couponAPI';
+import { COUPON_MAP } from '../coustants/couponInfo';
+import DisabledCover from './DisabledCover';
 import { Toast } from '@/shared/Toast';
 import { CouponType } from '../mocks/types/couponType';
 
@@ -9,56 +11,113 @@ interface EventCardProps {
   name: string;
   couponType: CouponType;
   stock: number;
-  endAt: string;
   point: number;
   description: string;
+  dueType: 'opened' | 'ended';
+  startDiff: number;
 }
 
 const EventCard = ({
-  couponId,
+  couponId, // TODO: 쿠폰 발급 요청을 위해 남겨둔건데, 쿠폰 이름으로 되있어서 확인 필요
   name,
-  // couponType,
+  couponType,
   stock,
-  // endAt,
-  // point,
-  description
+  point,
+  description,
+  dueType,
+  startDiff
 }: EventCardProps) => {
-  const handleGetCoupon = (couponId: number) => {
-    console.log(couponId, '요청');
-    console.log('쿠폰 발급 완료!'); // TODO: 쿠폰 받기 onSuccess > 토스트
+  const couponAvailable = startDiff === 0 && dueType === 'opened';
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: couponAPI.postCouponReceive,
+    onError: ({ response }) => {
+      if (response) {
+        const { status } = response;
+        switch (status) {
+          case 400:
+            Toast.show({ status: 'info', message: '쿠폰이 마감되었어요' });
+            break;
+          case 409:
+            Toast.show({ status: 'info', message: '이미 발급된 쿠폰이에요' });
+            break;
+          default:
+            Toast.show({ status: 'danger', message: '발급이 불가능합니다' });
+        }
+      }
+    },
+    onSuccess: () => {
+      Toast.show({ status: 'confirm', message: '쿠폰 발급 완료!' });
+    }
+  });
+
+  const handleGetCoupon = (couponName: string) => {
+    mutate({ couponName });
   };
 
   return (
     <div
       className={clsx(
-        'flex w-full shrink-0 flex-col overflow-hidden',
+        'relative flex w-full shrink-0 flex-col overflow-hidden',
         'rounded-xl bg-light-sub shadow-md dark:bg-dark-sub'
       )}
     >
-      <div className="relative flex h-32 items-center justify-center bg-dark-gray p-3">
-        <div
-          className={clsx(
-            'absolute right-3 top-3 w-fit rounded-2xl px-2 py-1',
-            'border border-warning text-xs text-warning'
-          )}
-        >
-          {stock}개 남음
-        </div>
-        <div className="flex items-center"></div>
+      <div className="relative flex h-48">
+        <img
+          className="object-cover"
+          src={COUPON_MAP[couponType].imgSrc}
+        ></img>
+        {dueType === 'opened' && (
+          <div
+            className={clsx(
+              'absolute right-3 top-3 w-fit rounded-2xl px-2 py-1',
+              'border border-light-gray text-xs text-light-gray',
+              'bg-black bg-opacity-[0.4]'
+            )}
+          >
+            {stock}개 남음
+          </div>
+        )}
       </div>
-      <div className="p-4">
-        <div className="mb-2 flex items-center justify-between">
-          <div className="font-IMHyemin-bold">{name}</div>
-          <div className="text-xs text-dark-gray">3일 뒤 종료</div>
+
+      <div className="flex flex-col gap-3 px-3 py-4">
+        <div className="flex flex-col gap-1">
+          <div className="font-IMHyemin-bold">
+            {name}
+            <span
+              className={clsx(
+                'ml-2 font-IMHyemin-bold',
+                COUPON_MAP[couponType].textStyle
+              )}
+            >
+              {COUPON_MAP[couponType].prefix}
+            </span>{' '}
+            {point}
+            {COUPON_MAP[couponType].unit}
+          </div>
+          <div className="text-xs text-dark-gray">{description}</div>
         </div>
-        <div className="mb-5 text-xs text-dark-gray">{description}</div>
-        <div
-          className="btn btn-light-point dark:btn-dark-point cursor-pointer text-center font-IMHyemin-bold"
-          onClick={() => handleGetCoupon(couponId)}
-        >
-          쿠폰 받기
+
+        <div className="flex items-center justify-end">
+          <div
+            className={clsx(
+              'cursor-pointer text-center font-IMHyemin-bold',
+              'btn px-7 py-1 text-sm text-white',
+              COUPON_MAP[couponType].bgStyle,
+              { 'pointer-events-none': isPending }
+            )}
+            onClick={() => handleGetCoupon(name)}
+          >
+            쿠폰 받기
+          </div>
         </div>
       </div>
+      {!couponAvailable && (
+        <DisabledCover
+          dueType={dueType}
+          startDiff={startDiff}
+        />
+      )}
     </div>
   );
 };
