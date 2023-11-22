f275c964e2b6577e5f68d9bbeff3bec2fcbdfaa2
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2023-11-22 16:43:13 +0900

    feat: 로그인 관련 리다이렉션 로직 작성 (#198)
    * feat: 401 상태코드 받으면 /join 페이지로 리다이렉션
    
    * feat: 앱 마운트시 로그인 확인 네트워크 요청
    
    * refactor: queryClient 로직을 다른 파일로 분리
    
    * refactor: 로그인 훅 App에서 분리
    
    * feat: 로컬 스토리지 관련 로직 추가
    
    * design: ErrorBoundary Fallback 컴포넌트에 텍스트 다크모드 색상

--- src/core/mocks/handlers/members.ts
+++ src/core/mocks/handlers/members.ts
@@ -2,6 +2,26 @@ import { http, HttpResponse, delay } from 'msw';
 import { baseURL } from '../baseURL';
 
 const membersHandlers = [
+  http.get(baseURL('/members'), async () => {
+    await delay(1000);
+
+    const status: number = 200;
+    let response = {};
+
+    switch (status) {
+      case 200:
+        response = {
+          message:
+            '현재 로그인 된 상태에요. 당신의 개인 Member 정보를 보내줄게요.'
+        };
+        break;
+      case 401:
+        response = { message: '로그인이 필요해요.' };
+        break;
+    }
+
+    return HttpResponse.json(response, { status });
+  }),
   http.post(baseURL('/members/login/kakao/oauth'), async () => {
     await delay(1000);
 
