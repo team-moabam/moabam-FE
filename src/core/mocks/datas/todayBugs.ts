56f239f2e0fa82bdd069ea4703594ce2d39fea02
Sejin Cha <62418379+chasj0326@users.noreply.github.com>
2023-11-22 18:35:26 +0900

    fix: 루틴페이지 bug api 호출 오류 수정 (#206)
    * fix: bug api 관련 오류 수정
    
    * fix: 테스트용 코드 원상복구
    
    * refactor: 사용하지 않는 모듈 import 삭제
    
    * fix: 삭제된 파일 관련 빌드 에러 수정
    
    * fix: 빌드 에러 수정

--- src/core/mocks/datas/todayBugs.ts
+++
@@ -1,6 +0,0 @@
-import { TodayBugs } from '@/core/types/TodayBugs';
-
-export const TODAY_BUGS: TodayBugs = {
-  morningBug: 9,
-  nightBug: 3
-};
--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -1,4 +1,3 @@
-export type { TodayBugs } from './TodayBugs';
 export type { MyJoinRoom } from './MyJoinRoom';
 export type { DayType } from './Room';
 export type { ParticipatingRoom } from './MyJoinRoom';
