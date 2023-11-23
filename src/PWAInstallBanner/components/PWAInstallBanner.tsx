00da4966f3b7ef6f549c6e65021547ee3b74097c
bbearcookie <aodem@naver.com>
2023-11-23 18:27:26 +0900

    feat: 설치 배너 열리고 닫히는 로직 작성

--- src/PWAInstallBanner/components/PWAInstallBanner.tsx
+++ src/PWAInstallBanner/components/PWAInstallBanner.tsx
@@ -1,25 +1,26 @@
 import { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Icon } from '@/shared/Icon';
 
 const PWAInstallBanner = () => {
   const deferredPrompt = useRef<any>(null);
-  const [show, setShow] = useState(false);
+  const [isShow, setIsShow] = useState(false);
 
-  const handleInstall = async () => {
+  const handleInstall = () => {
     deferredPrompt.current.prompt();
+  };
 
-    const { outcome } = await deferredPrompt.current.userChoice;
-
-    console.log(`User response to the install prompt: ${outcome}`);
+  const handleClose = () => {
+    setIsShow(false);
   };
 
   useEffect(() => {
     const handleBeforeInstallPrompt = (e: Event) => {
       e.preventDefault();
       deferredPrompt.current = e;
-      console.log(e);
-      console.log(`'beforeinstallprompt' event was fired.`);
+      setIsShow(true);
+      console.log('beforeinstallprompt Event fired', e);
     };
 
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
@@ -31,44 +32,67 @@ const PWAInstallBanner = () => {
       );
   }, []);
 
+  useEffect(() => {
+    const handleAppInstalled = () => {
+      setIsShow(false);
+      console.log('appinstalled Event fired');
+    };
+
+    window.addEventListener('appinstalled', handleAppInstalled);
+
+    return () => window.removeEventListener('appinstalled', handleAppInstalled);
+  }, []);
+
   return (
-    <div className="flex w-full justify-center">
-      <div
-        className={clsx(
-          'absolute bottom-20 z-pwaInstallBanner h-16 w-[90%]',
-          'rounded-lg font-bold text-black shadow-lg dark:text-white',
-          'flex items-center justify-center gap-2',
-          'bg-gradient-to-r from-light-point to-light-point-hover'
-        )}
-      >
-        <img
-          className="ml-4"
-          src="/logo.png"
-          width={50}
-          height={50}
-        />
-        <p className="grow">
-          앱으로 <b>설치</b>할 수도 있어요
-        </p>
-        <section className="me-4 flex gap-2">
+    <AnimatePresence>
+      {isShow && (
+        <motion.div
+          className="flex w-full justify-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
           <div
-            className={iconButtonStyle}
-            onClick={handleInstall}
+            className={clsx(
+              'absolute bottom-36 z-pwaInstallBanner h-16 w-[90%]',
+              'rounded-lg font-bold text-black shadow-lg dark:text-white',
+              'flex items-center justify-center gap-2',
+              'bg-gradient-to-r from-light-point to-light-point-hover'
+            )}
           >
-            <Icon
-              icon="MdFileDownload"
-              size="lg"
+            <img
+              className="ml-4"
+              src="/logo.png"
+              width={50}
+              height={50}
             />
+            <p className="grow">
+              앱으로 <b>설치</b>할 수도 있어요
+            </p>
+            <section className="me-4 flex gap-2">
+              <div
+                className={iconButtonStyle}
+                onClick={handleInstall}
+              >
+                <Icon
+                  icon="MdFileDownload"
+                  size="lg"
+                />
+              </div>
+              <div
+                className={iconButtonStyle}
+                onClick={handleClose}
+              >
+                <Icon
+                  icon="CgClose"
+                  size="lg"
+                />
+              </div>
+            </section>
           </div>
-          <div className={iconButtonStyle}>
-            <Icon
-              icon="CgClose"
-              size="lg"
-            />
-          </div>
-        </section>
-      </div>
-    </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
