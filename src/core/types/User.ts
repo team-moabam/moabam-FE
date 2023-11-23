43ac31450ec2c06c7d75afd25996bdaed8385852
nsr1349 <nsr1349@naver.com>
2023-11-23 13:19:40 +0900

    feat: MyUserType 정의

--- src/core/types/User.ts
+++ src/core/types/User.ts
@@ -1,4 +1,4 @@
-export interface userType {
+export interface UserType {
   nickname: string;
   intro: string;
   level: number;
@@ -15,4 +15,23 @@ export interface userType {
   golden_bug?: number;
   morning_bug?: number;
   night_bug?: number;
+}
+
+export interface MyUserType {
+  nickname: string;
+  intro: string;
+  level: number;
+  exp: number;
+  birds: {
+    MORNING_SKIN: string;
+    NIGHT_SKIN: string;
+  };
+  badges: {
+    name: string;
+    Success: boolean;
+  }[];
+  profile_image: string | undefined;
+  golden_bug: number;
+  morning_bug: number;
+  night_bug: number;
 }
