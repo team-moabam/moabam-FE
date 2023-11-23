50d8a7c5e98405d619f97b7bcdbff4b094d9e040
Sejin Cha <chasj0326@naver.com>
2023-11-23 18:19:08 +0900

    feat: 로딩 fallback 구현

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,11 +2,19 @@ import { Suspense } from 'react';
 import { ErrorBoundary } from '@suspensive/react';
 import RoomNavigate from '@/RoomNavigate/components/RoomNavigate';
 import { NetworkFallback } from '@/shared/ErrorBoundary';
+import RoomNavigateFallback from '@/RoomNavigate/components/RoomNavigateFallback';
+import { Deffered } from '@/shared/Deffered';
 
 const Room = () => {
   return (
     <ErrorBoundary fallback={<NetworkFallback />}>
-      <Suspense>
+      <Suspense
+        fallback={
+          <Deffered>
+            <RoomNavigateFallback />
+          </Deffered>
+        }
+      >
         <RoomNavigate />
       </Suspense>
     </ErrorBoundary>
