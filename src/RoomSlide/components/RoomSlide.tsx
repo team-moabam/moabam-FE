d8e621bd724414345ca7e8520e78fde2862f401a
Sejin Cha <chasj0326@naver.com>
2023-11-24 21:16:16 +0900

    feat: 토글 버튼 및 슬라이드 레이아웃 수정

--- src/RoomSlide/components/RoomSlide.tsx
+++ src/RoomSlide/components/RoomSlide.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from 'react';
 import { ErrorBoundary } from '@suspensive/react';
-import { DAY_TYPE } from '../constants/dayType';
 import RoomData from './RoomData';
 import RoomDataFallback from './RoomDataFallback';
 import { Deffered } from '@/shared/Deffered';
@@ -12,15 +11,8 @@ interface RoomSlideProps {
 }
 
 const RoomSlide = ({ roomType }: RoomSlideProps) => {
-  const { TITLE, START, END } = DAY_TYPE[roomType];
   return (
-    <div className="h-full overflow-auto p-8">
-      <div className="mb-5 flex items-end gap-3">
-        <div className="font-IMHyemin-bold text-xl">{TITLE}</div>
-        <div className="text-sm text-dark-gray">
-          {START} ~ {END}시
-        </div>
-      </div>
+    <div className="h-full overflow-auto px-8 pt-4">
       <ErrorBoundary fallback={<NetworkFallback />}>
         <Suspense
           fallback={
--- src/RoomSlide/components/SlideController.tsx
+++ src/RoomSlide/components/SlideController.tsx
@@ -11,15 +11,15 @@ interface SlideControllerProps {
 
 const SlideController = ({ control, onSwiper }: SlideControllerProps) => {
   return (
-    <div className="relative flex h-20 w-16 items-center overflow-hidden rounded-full">
+    <div className="relative flex w-11 items-center overflow-hidden rounded-full">
       <div
         className={clsx(
-          'absolute inset-y-0 my-auto h-[0.2rem] w-full rounded-full',
+          'absolute inset-y-0 my-auto h-1 w-11 rounded-full',
           'bg-light-point dark:bg-dark-point'
         )}
       ></div>
       <Swiper
-        className="mb-[0.15rem] h-6 w-10 cursor-pointer gap-0 overflow-visible"
+        className="h-5 w-6 cursor-pointer gap-0 overflow-visible"
         modules={[Controller]}
         controller={{ control }}
         onSwiper={onSwiper}
@@ -29,16 +29,16 @@ const SlideController = ({ control, onSwiper }: SlideControllerProps) => {
         <SwiperSlide className="flex justify-end">
           <div
             className={clsx(
-              'relative right-[0.02rem] h-full w-3 rounded-s-full',
-              'border-y-1 border-r-1  bg-white shadow-bottom'
+              'relative right-[0.02rem] h-full w-[0.625rem] rounded-s-full',
+              'border-y-1 border-r-1 bg-white'
             )}
           ></div>
         </SwiperSlide>
         <SwiperSlide className="flex justify-start">
           <div
             className={clsx(
-              'relative left-[0.02rem] h-full w-3 rounded-e-full',
-              'border-y-1 border-l-1  bg-white shadow-bottom'
+              'relative left-[0.02rem] h-full w-[0.625rem] rounded-e-full',
+              'border-y-1 border-l-1 bg-white '
             )}
           ></div>
         </SwiperSlide>
