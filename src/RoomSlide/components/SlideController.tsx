d89341683d0c534818ba85ceadc69b81cb21069c
Sejin Cha <62418379+chasj0326@users.noreply.github.com>
2023-11-24 22:07:16 +0900

    feat: 루틴 페이지 슬라이드 UI 개선 (#251)
    * feat: 스와이퍼 컨테이너 안넘어가게 수정 + 이벤트 배너 수정 + 토글 버튼 추가
    
    * feat: swiper 동기화 토글 슬라이더 구현
    
    * feat: 컴포넌트 분리 및 스타일 수정
    
    * feat: 토글 버튼 및 슬라이드 레이아웃 수정
    
    * feat: 슬라이드 컨트롤러 스타일 수정
    
    * feat: 스타일 수정
    
    * feat: 2차 스타일 수정
    
    * feat: 슬라이드 영역 패딩 수정

---
+++ src/RoomSlide/components/SlideController.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { SwiperClass } from 'swiper/react';
+import { clsx } from 'clsx';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Controller } from 'swiper/modules';
+
+interface SlideControllerProps {
+  control: SwiperClass | null;
+  onSwiper: React.Dispatch<React.SetStateAction<SwiperClass | null>>;
+}
+
+const SlideController = ({ control, onSwiper }: SlideControllerProps) => {
+  return (
+    <div className="relative flex w-12 items-center overflow-hidden rounded-full">
+      <div
+        className={clsx(
+          'absolute inset-0 m-auto h-1 w-10 rounded-full',
+          'bg-light-point dark:bg-dark-point'
+        )}
+      ></div>
+      <Swiper
+        className="h-5 w-7 cursor-pointer gap-0 overflow-visible"
+        modules={[Controller]}
+        controller={{ control }}
+        onSwiper={onSwiper}
+        slidesPerView={1}
+        dir="rtl"
+      >
+        <SwiperSlide className="flex justify-end">
+          <div
+            className={clsx(
+              'relative right-[0.02rem] h-full w-[0.625rem] rounded-s-full',
+              'border-b-2 border-r-1 border-t-1 border-light-gray bg-white'
+            )}
+          ></div>
+        </SwiperSlide>
+        <SwiperSlide className="flex justify-start">
+          <div
+            className={clsx(
+              'relative left-[0.02rem] h-full w-[0.625rem] rounded-e-full',
+              'border-b-2 border-l-1 border-t-1 border-light-gray bg-white'
+            )}
+          ></div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default SlideController;
--- src/RoomSlide/index.ts
+++ src/RoomSlide/index.ts
@@ -1,1 +1,4 @@
 export { default as RoomSlide } from './components/RoomSlide';
+export { default as SlideController } from './components/SlideController';
+export { default as useDayTypes } from './hooks/useDayTypes';
+export { default as DayInfo } from './components/DayInfo';
