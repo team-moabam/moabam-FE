61bc142c8689f6cb3e5c46f2fdebf6d0598296a5
Sejin Cha <chasj0326@naver.com>
2023-11-24 17:13:22 +0900

    feat: 스와이퍼 컨테이너 안넘어가게 수정 + 이벤트 배너 수정 + 토글 버튼 추가

--- src/pages/RoutinesPage.tsx
+++ src/pages/RoutinesPage.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import { Suspense } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Controller } from 'swiper/modules';
+import { SwiperClass } from 'swiper/react';
 import { useTheme } from '@/core/hooks';
 import RoomSlide from '@/RoomSlide/components/RoomSlide';
 import { EventBanner } from '@/Promotion';
@@ -12,9 +15,24 @@ const RoutinesPage = () => {
       ? (['NIGHT', 'MORNING'] as const)
       : (['MORNING', 'NIGHT'] as const);
 
+  const [routineSwiper, setRoutineSwiper] = useState<SwiperClass | null>(null);
+  const [controllSwiper, setControllSwiper] = useState<SwiperClass | null>(
+    null
+  );
+  const [isFirstSlide, setIsFirstSlide] = useState(true);
+
   return (
     <div className="flex h-full flex-col overflow-auto">
-      <Swiper className="h-full w-full">
+      <Swiper
+        className="h-full w-full"
+        modules={[Controller]}
+        controller={{ control: controllSwiper }}
+        onSwiper={setRoutineSwiper}
+        allowSlidePrev={!isFirstSlide}
+        allowSlideNext={isFirstSlide}
+        onReachEnd={() => setIsFirstSlide(false)}
+        onReachBeginning={() => setIsFirstSlide(true)}
+      >
         {DAY_TYPES.map((roomType) => (
           <SwiperSlide
             className="h-full"
@@ -24,6 +42,23 @@ const RoutinesPage = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <Swiper
+        className="h-20 w-40 gap-0 rounded-full bg-white"
+        modules={[Controller]}
+        controller={{ control: routineSwiper }}
+        onSwiper={setControllSwiper}
+        slidesPerView={1}
+        allowSlidePrev={!isFirstSlide}
+        allowSlideNext={isFirstSlide}
+      >
+        <SwiperSlide className="flex items-start">
+          <div className="h-full w-full  bg-light-point"></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-full w-full bg-dark-point"></div>
+        </SwiperSlide>
+      </Swiper>
+
       <PWAInstallBanner />
       <Suspense>
         <EventBanner />
