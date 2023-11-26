403f00648082cd5d05317f5adf00afdefa06f7c2
Park Nayeon <within197@naver.com>
2023-11-26 21:06:09 +0900

    feat: 인증진척도 시간 제한 추가

--- src/RoomDetail/components/CertificationProgress.tsx
+++ src/RoomDetail/components/CertificationProgress.tsx
@@ -1,32 +1,45 @@
+import { useContext } from 'react';
+import makeCertifyTime from '../utils/makeCertifyTime';
+import { DateRoomDetailContext } from './RoomDetailProvider';
 import { ProgressBar } from '@/shared/ProgressBar';
 
 interface CertificationProgressProps {
   percentage: number;
   chooseDate: Date;
-  serverTime: Date;
+  certifyTime: number;
 }
 
 const CertificationProgress = ({
   percentage,
   chooseDate,
-  serverTime
+  certifyTime
 }: CertificationProgressProps) => {
+  const { serverTime } = useContext(DateRoomDetailContext);
+  const isTodayRoom = chooseDate.getDate() === serverTime.getDate();
+  const { nowTime, certificateStartTime } = makeCertifyTime(
+    certifyTime,
+    serverTime
+  );
+
+  const chooseMonthText = chooseDate.getMonth() + 1;
+  const chooseDateText = chooseDate.getDate();
+  const certificateProgress =
+    isTodayRoom && nowTime < certificateStartTime ? 0 : percentage;
+
   return (
     <div className="mb-[0.75rem]">
       <div className="flex items-end  justify-between pb-2">
         <h4 className="text-base text-black dark:text-white">
-          {chooseDate.getDate() === serverTime.getDate()
+          {isTodayRoom
             ? '오늘의 방 인증율'
-            : `${
-                chooseDate.getMonth() + 1
-              }월 ${chooseDate.getDate()}일 방 인증율`}
+            : `${chooseMonthText}월 ${chooseDateText}일 방 인증율`}
         </h4>
         <span className="text-2xl text-light-point dark:text-dark-point">
-          {percentage}%
+          {certificateProgress}%
         </span>
       </div>
       <ProgressBar
-        progress={percentage}
+        progress={certificateProgress}
         className="rounded-full"
       />
     </div>
