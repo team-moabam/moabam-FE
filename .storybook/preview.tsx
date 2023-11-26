9df533efde159be81af7b629f2701d74d6bca634
bbearcookie <aodem@naver.com>
2023-11-26 17:06:43 +0900

    chore: withRouter를 스토리북 전역에 설정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Preview } from '@storybook/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { initialize, mswLoader } from 'msw-storybook-addon';
+import { withRouter } from 'storybook-addon-react-router-v6';
 import queryClient from '../src/core/api/queryClient';
 import 'swiper/css';
 import 'swiper/css/bundle';
@@ -12,6 +13,7 @@ initialize();
 const preview: Preview = {
   loaders: [mswLoader],
   decorators: [
+    withRouter,
     (Story) => (
       <QueryClientProvider client={queryClient}>
         <Story />
