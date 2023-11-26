b71d6633f03e7c324ce3827e5e4140940c9ffc0b
bbearcookie <aodem@naver.com>
2023-11-26 17:34:12 +0900

    test: mswLoader 주석처리

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { initialize, mswLoader } from 'msw-storybook-addon';
+// import { initialize, mswLoader } from 'msw-storybook-addon';
 import { withRouter } from 'storybook-addon-react-router-v6';
 import queryClient from '../src/core/api/queryClient';
 import 'swiper/css';
 import 'swiper/css/bundle';
 import '../src/main.css';
 
-initialize();
+// initialize();
 
 const preview: Preview = {
-  loaders: [mswLoader],
+  // loaders: [mswLoader],
   decorators: [
     withRouter,
     (Story) => (
