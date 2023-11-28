4bd138e497883a29bc73dc5e750be6d88b6c28d9
nsr1349 <nsr1349@naver.com>
2023-11-28 14:13:36 +0900

    feat: 최종수정

--- src/MyCoupon/components/MyCouponList.tsx
+++ src/MyCoupon/components/MyCouponList.tsx
@@ -51,30 +51,33 @@ const MyCouponList = () => {
 
   return (
     <div className="p-5">
-      <div className="mb-5">쿠폰 ({data.length})</div>
-      <ul>
-        {data &&
-          data.map(({ couponId, description, name, point, type }) => (
-            <li
-              className="mb-2 flex rounded-lg bg-light-sub pr-0 shadow-lg dark:bg-dark-sub"
-              key={couponId}
-              onClick={() => handleUseCoupon(couponId, point, type)}
-            >
-              <div className="flex-1 p-5">
-                <p className="text-sm text-dark-gray">{description}</p>
-                <h1 className="mb-5 mt-1 text-xl">
-                  {type === 'DISCOUNT_COUPON'
-                    ? `${point}원 결제 할인`
-                    : `${bugType[type]}벌레 ${point}마리`}
-                </h1>
-                <p className="text-sm text-dark-gray">{name}</p>
-              </div>
-              <div className="grid place-content-center border-l border-dark-gray">
-                <div className="rotate-90 text-dark-gray">coupon</div>
-              </div>
-            </li>
-          ))}
-      </ul>
+      {data && (
+        <>
+          <div className="mb-5">쿠폰 ({data.length})</div>
+          <ul>
+            {data.map(({ couponId, description, name, point, type }) => (
+              <li
+                className="mb-2 flex rounded-lg bg-light-sub pr-0 shadow-lg dark:bg-dark-sub"
+                key={couponId}
+                onClick={() => handleUseCoupon(couponId, point, type)}
+              >
+                <div className="flex-1 p-5">
+                  <p className="text-sm text-dark-gray">{description}</p>
+                  <h1 className="mb-5 mt-1 text-xl">
+                    {type === 'DISCOUNT_COUPON'
+                      ? `${point}원 결제 할인`
+                      : `${bugType[type]}벌레 ${point}마리`}
+                  </h1>
+                  <p className="text-sm text-dark-gray">{name}</p>
+                </div>
+                <div className="grid place-content-center border-l border-dark-gray">
+                  <div className="rotate-90 text-dark-gray">coupon</div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
