80fac05a23ce02bb1f3906ed78171aeda59978b9
nsr1349 <nsr1349@naver.com>
2023-11-30 04:00:51 +0900

    feat: order log 타입 반영

--- src/OrderLogList/components/OrderLogList.tsx
+++ src/OrderLogList/components/OrderLogList.tsx
@@ -2,8 +2,8 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { RiCoupon2Fill } from 'react-icons/ri';
 import { FaClipboardList } from 'react-icons/fa6';
 import { IoMdCash } from 'react-icons/io';
+import { GiPayMoney } from 'react-icons/gi';
 import { bugOptions } from '@/core/api/options';
-import { bugs } from '../mocks/Bugs';
 
 const OrderLogList = () => {
   const {
@@ -24,28 +24,61 @@ const OrderLogList = () => {
 
   return (
     <ul>
-      {history.map(({ id, bugType, actionType, amount, date }) => (
-        <li
-          className="flex items-center p-5"
-          key={id}
-        >
-          <div className="flex flex-1 items-center gap-5">
-            <div className={`text-xl ${bugs[bugType].color} `}>
-              {actionsIcon[actionType]}
+      {history.map(({ id, bugType, actionType, date, quantity }) => {
+        const { icon, StartText, ResultText } = historyOptions[actionType];
+        return (
+          <li
+            className="flex items-center p-5"
+            key={id}
+          >
+            <div className="flex flex-1 items-center gap-5">
+              <div className={`text-xl ${bugs[bugType].color} `}>{icon}</div>
+              <div>{`${StartText} ${bugs[bugType].name} ${quantity}마리 ${ResultText}`}</div>
             </div>
-            <div>{`${actionType}으로 ${bugs[bugType].name} ${amount}마리 획득!`}</div>
-          </div>
-          <div className="text-sm text-dark-gray">{date.slice(0, 10)}</div>
-        </li>
-      ))}
+            <div className="text-sm text-dark-gray">{date.slice(0, 10)}</div>
+          </li>
+        );
+      })}
     </ul>
   );
 };
 
 export default OrderLogList;
 
-const actionsIcon = {
-  보상: <FaClipboardList />,
-  쿠폰: <RiCoupon2Fill />,
-  충전: <IoMdCash />
+const historyOptions = {
+  REWARD: {
+    icon: <FaClipboardList />,
+    StartText: '인증 보상으로',
+    ResultText: '획득!'
+  },
+  COUPON: {
+    icon: <RiCoupon2Fill />,
+    StartText: '쿠폰으로',
+    ResultText: '획득!'
+  },
+  CHARGE: {
+    icon: <IoMdCash />,
+    StartText: '충전으로',
+    ResultText: '획득!'
+  },
+  USE: {
+    icon: <GiPayMoney />,
+    StartText: '',
+    ResultText: '사용!'
+  }
+};
+
+const bugs = {
+  MORNING: {
+    color: 'text-light-point',
+    name: '낮벌레'
+  },
+  NIGHT: {
+    color: 'text-dark-point',
+    name: '밤벌레'
+  },
+  GOLDEN: {
+    color: 'text-warning',
+    name: '황금벌레'
+  }
 };
--- src/core/types/BugHistory.ts
+++ src/core/types/BugHistory.ts
@@ -2,15 +2,14 @@ export interface BugHistory {
   history: {
     id: number;
     bugType: 'MORNING' | 'NIGHT' | 'GOLDEN';
-    actionType: '보상' | '쿠폰' | '충전';
-    amount: number;
+    actionType: 'USE' | 'REWARD' | 'CHARGE' | 'COUPON';
+    quantity: number;
     date: string;
     paymentInfo?: {
       id: number;
-      count: number;
-      price: number;
-      discount: number;
-      provider: string;
+      orderName: string;
+      discountAmount: number;
+      totalAmount: number;
     };
   }[];
 }
