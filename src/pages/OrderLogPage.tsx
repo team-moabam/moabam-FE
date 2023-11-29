0a19eac63e700b4e3b022713eee1020128370ba2
nsr1349 <nsr1349@naver.com>
2023-11-29 15:34:42 +0900

    feat: 사용내역 으로 변경

--- src/pages/OrderLogPage.tsx
+++ src/pages/OrderLogPage.tsx
@@ -9,7 +9,7 @@ const OrderLogPage = () => {
       <div className="h-full overflow-auto">
         <Header
           prev="myPage"
-          title="구매 내역"
+          title="사용 내역"
           className="sticky top-0 bg-light-main dark:bg-dark-main"
         />
         <Suspense fallback={<OrderLogListFallback />}>
