e8ca5dcd57bf0bff1be4b6da9a34c8a7f3cbe099
Sejin Cha <62418379+chasj0326@users.noreply.github.com>
2023-11-30 03:29:41 +0900

    fix: RoutinesPage 두번 로드되는 현상 수정 (#363)

--- src/StartSlide/index.ts
+++ src/StartSlide/index.ts
@@ -2,3 +2,4 @@ export { default as SwipeArrow } from './components/SwipeArrow';
 export { default as Background } from './components/Background';
 export { default as UserInfo } from './components/UserInfo';
 export { default as UserInfoFallback } from './components/UserInfoFallback';
+export { default as FakeRoutinesPage } from './components/FakeRoutinesPage';
