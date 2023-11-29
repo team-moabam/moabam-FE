d74b301ecd3a45a1590fddf07d71eca0b11d45f2
nsr1349 <nsr1349@naver.com>
2023-11-30 04:26:02 +0900

    feat: product sheet 개선

--- src/core/api/options/item.ts
+++ src/core/api/options/item.ts
@@ -2,7 +2,7 @@ import { queryOptions } from '@tanstack/react-query';
 import itemAPI from '../functions/itemAPI';
 
 export const itemOptions = {
-  all: (type: string) =>
+  all: (type: 'MORNING' | 'NIGHT' | 'GOLDEN') =>
     queryOptions({
       queryKey: ['item', type] as const,
       queryFn: () => itemAPI.all(type)
