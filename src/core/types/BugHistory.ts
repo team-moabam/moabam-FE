8d7b97fafdcc7c4d055bad00329965c53031e2dd
nsr1349 <nsr1349@naver.com>
2023-11-29 15:32:14 +0900

    feat: 액션 타입으로 변경

--- src/core/types/BugHistory.ts
+++ src/core/types/BugHistory.ts
@@ -2,7 +2,7 @@ export interface BugHistory {
   history: {
     id: number;
     bugType: 'MORNING_BUG' | 'NIGHT_BUG' | 'GOLDEN_BUG';
-    action: '보상' | '쿠폰' | '충전';
+    actionType: '보상' | '쿠폰' | '충전';
     amount: number;
     date: string;
     paymentInfo?: {
