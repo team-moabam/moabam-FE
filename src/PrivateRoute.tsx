0a8e628052478e200b0ec249d9fbb49419fb7b01
Sejin Cha <chasj0326@naver.com>
2023-11-30 02:05:12 +0900

    feat: param 값 검사 로직 및 페이지 작성

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -1,8 +1,18 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { STORAGE_KEYS } from './core/constants/storageKeys';
+import { useRouteData } from './core/hooks';
+import InvalidPage from '@/pages/InvalidPage';
 
 const PrivateRoute = () => {
   const memberId = localStorage.getItem(STORAGE_KEYS.MEMBER_ID);
+  const { params } = useRouteData();
+
+  for (const [paramName, paramValue] of Object.entries(params)) {
+    if (isNaN(Number(paramValue))) {
+      return <InvalidPage paramName={paramName} />;
+    }
+  }
+
   return memberId ? <Outlet /> : <Navigate to="/join" />;
 };
 
