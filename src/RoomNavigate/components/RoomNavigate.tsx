4a3bcf8c71d2204b1c8d628a725c4bfca404a05e
Sejin Cha <62418379+chasj0326@users.noreply.github.com>
2023-12-01 06:02:19 +0900

    feat: Room Navigate 로직 수정 (#395)
    * feat: 이동할 방 계산 로직 수정
    
    * feat: 스토리지 저장 추가
    
    * refactor: 세션스토리지 키 상수화
    
    * fix: certifyingRoom 계산 오류 수정

--- src/RoomNavigate/components/RoomNavigate.tsx
+++ src/RoomNavigate/components/RoomNavigate.tsx
@@ -4,7 +4,7 @@ import timeOption from '@/core/api/options/time';
 import { roomOptions } from '@/core/api/options';
 import { MyJoinRoom } from '@/core/types';
 import { useMoveRoute } from '@/core/hooks';
-import pickRoomToShow from '../utils/pickRoomToShow';
+import pickRoom from '../utils/pickRoom';
 import NoRoom from './NoRoom';
 
 const RoomNavigate = () => {
@@ -20,10 +20,20 @@ const RoomNavigate = () => {
   });
 
   useEffect(() => {
-    const roomToMove = pickRoomToShow(data, today);
-    if (roomToMove) {
-      moveTo('roomDetail', { roomId: roomToMove.roomId }, { replace: true });
+    if (data.length === 0) return;
+
+    const { certifyingRoom, latestRoom, closestRoom } = pickRoom(data, today);
+    const roomParam: { roomId: string | number } = { roomId: '' };
+
+    if (certifyingRoom) {
+      roomParam.roomId = certifyingRoom;
+    } else if (latestRoom) {
+      roomParam.roomId = latestRoom;
+    } else {
+      roomParam.roomId = closestRoom;
     }
+
+    moveTo('roomDetail', roomParam, { replace: true });
   }, [data, moveTo, today]);
 
   return <NoRoom />;
