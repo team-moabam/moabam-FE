0163ff76412cda5e43aaef1291bda4d80dd99a2b
Park Na Yeon <65156388+nayeon-hub@users.noreply.github.com>
2023-11-30 16:09:45 +0900

    fix: 토스트 에러 메세지 수정 (#375)

--- src/RoomDetail/components/RoomMember.tsx
+++ src/RoomDetail/components/RoomMember.tsx
@@ -55,7 +55,7 @@ const RoomMember = ({
       .catch((err) => {
         Toast.show({
           status: 'danger',
-          message: err.message
+          message: err.response.data
         });
       });
   };
