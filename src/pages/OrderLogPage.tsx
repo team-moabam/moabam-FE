1264173fc54370f92a996e0f5a883f0450549a5b
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2023-12-02 17:50:07 +0900

    refactor: 헤더의 뒤로가기가 갇히는 문제 수정 (#412)
    * refactor: Header 컴포넌트에서 뒤로 갈 페이지가 모아밤이 아니면 main으로 가는 로직 작성
    
    * refactor: 모든 헤더의 뒤로가기를 history stack 기반으로 -1 하도록 수정
    
    * refactor: prev를 boolean으로 변경
    
    ---------
    
    Co-authored-by: Sejin Cha <62418379+chasj0326@users.noreply.github.com>

--- src/pages/OrderLogPage.tsx
+++ src/pages/OrderLogPage.tsx
@@ -9,7 +9,7 @@ const OrderLogPage = () => {
     <>
       <div className="h-full overflow-auto">
         <Header
-          prev="myPage"
+          prev
           title="사용 내역"
           className="sticky top-0 bg-light-main dark:bg-dark-main"
         />
