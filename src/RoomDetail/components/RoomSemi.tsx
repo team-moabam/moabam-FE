9a5ae2ffb4ceaa9ce5fd3b8e9b9bc8d251f6c24b
Sejin Cha <chasj0326@naver.com>
2023-12-03 00:44:43 +0900

    fix: 메인 코드 내용 복구

--- src/RoomDetail/components/RoomSemi.tsx
+++ src/RoomDetail/components/RoomSemi.tsx
@@ -70,7 +70,7 @@ const RoomSemi = ({ roomId, serverTime, checkedRoomJoin }: RoomSemiProps) => {
                 <div
                   className={twMerge(
                     clsx(
-                      'relative mb-[0.22rem] h-[3.9rem] w-[3.25rem] bg-contain bg-no-repeat ',
+                      'relative mb-[0.22rem] h-[3.9rem] w-[3.25rem] bg-contain bg-bottom bg-no-repeat',
                       {
                         "after:absolute after:right-[-14px] after:top-[-10px] after:block after:content-['zzz'] after:origin-center after:rotate-[-16deg]":
                           isBirdSleep,
