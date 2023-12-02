1e16b693eca7612bceec5ba75d30662fc68ee857
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2023-12-02 17:45:46 +0900

    refactor: 로그인 되어있어도 PublicRoute에 접근 가능하도록 수정 (#410)
    Co-authored-by: Sejin Cha <62418379+chasj0326@users.noreply.github.com>

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,3 +1,6 @@
+import { Navigate } from 'react-router-dom';
+import { STORAGE_KEYS } from '@/core/constants/storageKeys';
+
 const baseURI = import.meta.env.PROD
   ? import.meta.env.VITE_DEPLOY_ENDPOINT
   : import.meta.env.VITE_LOCALHOST;
@@ -10,6 +13,12 @@ const KAKAO_LOGIN_URL =
   `/login/kakao/oauth`;
 
 const JoinPage = () => {
+  const memberId = localStorage.getItem(STORAGE_KEYS.MEMBER_ID);
+
+  if (memberId) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <main className="flex h-full flex-col items-center justify-center p-2">
       <section className="flex grow flex-col items-center justify-center text-center">
