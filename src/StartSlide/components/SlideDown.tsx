810d86d50e1de43e2d102b35cfe5bed1cd42a01f
Sejin Cha <chasj0326@naver.com>
2023-12-03 05:06:20 +0900

    feat: 터치 이벤트 연결

--- src/StartSlide/components/SlideDown.tsx
+++ src/StartSlide/components/SlideDown.tsx
@@ -22,6 +22,11 @@ const SlideDown = ({
     setIsDragging(true);
     setStartY(e.clientY);
   };
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setIsDragging(true);
+    setStartY(e.changedTouches[0].pageY);
+  };
+
   const handleMouseMove = (e: React.MouseEvent) => {
     if (isDragging && startY !== null && slideRef.current) {
       const movedY = e.clientY - startY;
@@ -29,14 +34,21 @@ const SlideDown = ({
       setCurrentY((movedY / fullHeight) * 100);
     }
   };
-  const handleMouseUp = () => {
+  const handleTouchMove = (e: React.TouchEvent) => {
+    if (isDragging && startY !== null && slideRef.current) {
+      const movedY = e.changedTouches[0].pageY - startY;
+      const fullHeight = slideRef.current.parentElement?.clientHeight || 0;
+      setCurrentY((movedY / fullHeight) * 100);
+    }
+  };
+
+  const handleSlideEnd = () => {
     if (isDragging) {
       setIsDragging(false);
       setStartY(null);
       setCurrentY((prev) => (prev > 40 ? fullPercentage : 0));
     }
   };
-
   useEffect(() => {
     if (currentY === fullPercentage) {
       const timeoutId = setTimeout(() => {
@@ -58,8 +70,12 @@ const SlideDown = ({
       }}
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseUp}
+      onMouseUp={handleSlideEnd}
+      onMouseLeave={handleSlideEnd}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleSlideEnd}
+      onTouchCancel={handleSlideEnd}
       className={className}
     >
       {children}
