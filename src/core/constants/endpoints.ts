88d1d2ad697b59336f30ea1691e01cbb8f7c8944
bbearcookie <aodem@naver.com>
2023-12-03 21:24:43 +0900

    chore: 클라이언트 배포 경로를 상수 파일에 관리

--- src/core/constants/endpoints.ts
+++ src/core/constants/endpoints.ts
@@ -1,4 +1,15 @@
+// 백엔드 API 경로
 export const BACKEND_API_ENDPOINT =
   import.meta.env.VITE_DEPLOY_TARGET === 'production'
     ? import.meta.env.VITE_PROD_BACKEND_API_ENDPOINT
     : import.meta.env.VITE_DEV_BACKEND_API_ENDPOINT;
+
+// 클라이언트 배포 경로
+// 로컬에서는 VITE_LOCALHOST
+// 개발서버에서는 VITE_PROD_DEPLOY_ENDPOINT
+// 프로덕션에서는 VITE_DEV_DEPLOY_ENDPOINT
+export const CLIENT_DEPLOY_ENDPOINT = import.meta.env.PROD
+  ? import.meta.env.VITE_DEPLOY_TARGET === 'production'
+    ? import.meta.env.VITE_PROD_DEPLOY_ENDPOINT
+    : import.meta.env.VITE_DEV_DEPLOY_ENDPOINT
+  : import.meta.env.VITE_LOCALHOST;
