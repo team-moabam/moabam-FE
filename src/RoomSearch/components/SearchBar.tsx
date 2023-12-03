198a20f68a1bcff22df2c22ef152a26504af1c86
bbearcookie <aodem@naver.com>
2023-12-03 18:53:47 +0900

    refactor: 검색바 디바운싱에 새로운 유틸 함수 적용

--- src/RoomSearch/components/SearchBar.tsx
+++ src/RoomSearch/components/SearchBar.tsx
@@ -1,22 +1,26 @@
+import { useMemo } from 'react';
+import { debounce } from '@/core/utils/debounce';
 import { Input } from '@/shared/Input';
-import useDebounceSearch from '../hooks/useDebounceSearch';
 
 interface SearchBarProps {
   onSearch: (input: string) => void;
 }
 
 const SearchBar = ({ onSearch }: SearchBarProps) => {
-  const debounceSearch = useDebounceSearch({
-    debounce: 500,
-    onSearch
-  });
+  const handleDebouncedSearch = useMemo(
+    () =>
+      debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+        onSearch(e.target.value);
+      }, 500),
+    [onSearch]
+  );
 
   return (
     <div>
       <Input
         className="rounded-2xl pl-4 dark:bg-dark-sub"
         placeholder="방 제목, 루틴 등을 써보세요"
-        onChange={(e) => debounceSearch(e.target.value)}
+        onChange={handleDebouncedSearch}
       />
     </div>
   );
