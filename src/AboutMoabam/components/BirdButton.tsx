a7234500f406ae888b738736d97b07bf798b95c3
Sejin Cha <chasj0326@naver.com>
2023-12-05 00:15:22 +0900

    feat: PC view 버튼에 각종 페이지 링크 연결

--- src/AboutMoabam/components/BirdButton.tsx
+++ src/AboutMoabam/components/BirdButton.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 interface BirdButtonProps {
   text: string;
   birdImage: string;
+  link: string;
 }
 
-const BirdButton = ({ text, birdImage }: BirdButtonProps) => {
+const BirdButton = ({ text, birdImage, link }: BirdButtonProps) => {
   return (
     <div className="flex flex-col items-center gap-8">
-      <div className="btn btn-light-point dark:btn-dark-point cursor-pointer text-lg dark:text-dark-main max-xl:text-base">
+      <a
+        href={link}
+        target="_blank"
+        className="btn btn-light-point dark:btn-dark-point cursor-pointer text-lg dark:text-dark-main max-xl:text-base"
+      >
         {text}
-      </div>
+      </a>
       <div>
         <img
           src={birdImage}
