6cb924d0bbe26c674bd4921108d6300dfa3c1f89
bbearcookie <aodem@naver.com>
2024-01-25 17:10:41 +0900

    refactor: 퍼널 컴포넌트 리팩토링

--- src/shared/Funnel/components/Step.tsx
+++ src/shared/Funnel/components/Step.tsx
@@ -1,13 +1,11 @@
-import { PropsWithChildren } from 'react';
-import { StepNames } from '../types/funnel';
-
-interface StepProps<T extends StepNames> {
+export interface StepProps<T extends readonly string[]> {
+  /** Funnel 컴포넌트에서 렌더링 할 Step을 필터링하는데 사용됩니다. */
   name: T[number];
+  children: React.ReactNode;
 }
 
-const Step = <T extends StepNames>({
-  children
-}: PropsWithChildren<StepProps<T>>) => {
+/** 퍼널에서 스텝 별로 렌더링 할 컴포넌트입니다. */
+const Step = <T extends readonly string[]>({ children }: StepProps<T>) => {
   return <>{children}</>;
 };
 
--- src/shared/Funnel/index.ts
+++ src/shared/Funnel/index.ts
@@ -1,2 +1,2 @@
-export { default as Funnel } from './components/Funnel';
+export { default as createFunnel } from './utils/createFunnel';
 export { default as useFunnel } from './hooks/useFunnel';
