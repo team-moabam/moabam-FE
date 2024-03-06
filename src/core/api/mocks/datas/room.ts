28dfb0d13f313abd35f55a075d160bb1b8023e7d
SangHoon Lee <50488780+bbearcookie@users.noreply.github.com>
2024-03-06 19:59:37 +0900

    fix: 방 상세 목 데이터 이미지 수정 (#511)

--- src/core/api/mocks/datas/room.ts
+++ src/core/api/mocks/datas/room.ts
@@ -3,7 +3,7 @@ export const RoomInfo = {
   roomCreatedAt: '2023-11-28T13:36:49.783842',
   title: '윤명이의 루틴방',
   managerNickName: '오목눈이#U8WN6I',
-  roomImage: 'https://image.moabam.com/moabam/default/room-level-00.png',
+  roomImage: '/assets/background/room-level-03.png',
   level: 5,
   currentExp: 4,
   totalExp: 80,
@@ -35,13 +35,10 @@ export const RoomInfo = {
       memberId: 19,
       nickname: '오목눈이#U8WN6I',
       isNotificationSent: false,
-      profileImage:
-        'https://image.moabam.com/moabam/default/member-profile.png',
+      profileImage: '/assets/user.png',
       contributionPoint: 100,
-      awakeImage:
-        'https://image.moabam.com/moabam/skins/omok/default/eyes-opened.png',
-      sleepImage:
-        'https://image.moabam.com/moabam/skins/omok/default/eyes-closed.png',
+      awakeImage: '/assets/skins/awakeOmokSkin0.png',
+      sleepImage: '/assets/skins/sleepOmokSkin0.png',
       certificationImage: null
     },
     {
@@ -49,27 +46,26 @@ export const RoomInfo = {
       memberId: 109,
       nickname: '오목눈이#U8WN6H',
       isNotificationSent: true,
-      profileImage:
-        'https://image.moabam.com/moabam/default/member-profile.png',
+      profileImage: '/assets/user.png',
       contributionPoint: 100,
-      awakeImage: 'https://image.moabam.com/moabam/skins/omok/default/egg.png',
-      sleepImage: 'https://image.moabam.com/moabam/skins/omok/default/egg.png',
+      awakeImage: '/assets/skins/eggOmok.png',
+      sleepImage: '/assets/skins/eggOmok.png',
       certificationImage: {
         images: [
           {
             routineId: 8,
             image:
-              'https://image.moabam.com/certifications/2023-11-30/8_e15719e3-83f8-4cdc-b4a6-5511290d8a4a.png'
+              'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202204/07/dd8b47d5-1c8b-489c-bb98-feadc613cf62.jpg'
           },
           {
             routineId: 9,
             image:
-              'https://image.moabam.com/certifications/2023-11-30/9_a78ddf52-854c-4a89-af6b-96b1ff418997.png'
+              'https://img.hani.co.kr/imgdb/resize/2016/0318/145820136237_20160318.JPG'
           },
           {
             routineId: 10,
             image:
-              'https://image.moabam.com/certifications/2023-11-30/10_0c0046d4-76e2-43cb-8e2e-0e4adafd45bf.png'
+              'https://cdn.kormedi.com/wp-content/uploads/2023/06/unnamed-file-152-700x467.jpg.webp'
           }
         ]
       }
@@ -79,29 +75,26 @@ export const RoomInfo = {
       memberId: 5,
       nickname: '오목눈이#U8WN6J',
       isNotificationSent: false,
-      profileImage:
-        'https://image.moabam.com/moabam/default/member-profile.png',
+      profileImage: '/assets/user.png',
       contributionPoint: 100,
-      awakeImage:
-        'https://image.moabam.com/moabam/skins/omok/santa/eyes-opened.png',
-      sleepImage:
-        'https://image.moabam.com/moabam/skins/omok/santa/eyes-closed.png',
+      awakeImage: '/assets/skins/awakeOmokSkin3.png',
+      sleepImage: '/assets/skins/sleepOmokSkin3.png',
       certificationImage: {
         images: [
           {
             routineId: 8,
             image:
-              'https://image.moabam.com/certifications/2023-11-30/8_e15719e3-83f8-4cdc-b4a6-5511290d8a4a.png'
+              'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202204/07/dd8b47d5-1c8b-489c-bb98-feadc613cf62.jpg'
           },
           {
             routineId: 9,
             image:
-              'https://image.moabam.com/certifications/2023-11-30/9_a78ddf52-854c-4a89-af6b-96b1ff418997.png'
+              'https://img.hani.co.kr/imgdb/resize/2016/0318/145820136237_20160318.JPG'
           },
           {
             routineId: 10,
             image:
-              'https://image.moabam.com/certifications/2023-11-30/10_0c0046d4-76e2-43cb-8e2e-0e4adafd45bf.png'
+              'https://cdn.kormedi.com/wp-content/uploads/2023/06/unnamed-file-152-700x467.jpg.webp'
           }
         ]
       }
@@ -137,19 +130,19 @@ export const RoomInfoBeforeEditing = {
       memberId: 4,
       nickname: '참여자1',
       contributionPoint: 70,
-      profileImage: 'https://~'
+      profileImage: '/assets/user.png'
     },
     {
       memberId: 6,
       nickname: '방장',
       contributionPoint: 99,
-      profileImage: 'https://~'
+      profileImage: '/assets/user.png'
     },
     {
       memberId: 8,
       nickname: '참여자2',
       contributionPoint: 70,
-      profileImage: 'https://~'
+      profileImage: '/assets/user.png'
     }
   ]
 };
@@ -182,19 +175,19 @@ export const RoomInfoBeforeEditing2 = {
       memberId: 4,
       nickname: '참여자1',
       contributionPoint: 70,
-      profileImage: 'https://~'
+      profileImage: '/assets/user.png'
     },
     {
       memberId: 6,
       nickname: '방장',
       contributionPoint: 99,
-      profileImage: 'https://~'
+      profileImage: '/assets/user.png'
     },
     {
       memberId: 8,
       nickname: '참여자2',
       contributionPoint: 70,
-      profileImage: 'https://~'
+      profileImage: '/assets/user.png'
     }
   ]
 };
@@ -203,7 +196,7 @@ export const RoomSemiInfo = {
   roomId: 3,
   isPassword: true,
   title: '하윙 방',
-  roomImage: 'https://~',
+  roomImage: '/assets/background/room-level-00.png',
   level: 3,
   currentExp: 4,
   totalExp: 80,
@@ -231,15 +224,15 @@ export const RoomSemiInfo = {
       rank: 1,
       memberId: 3,
       nickname: '오목이123',
-      awakeImage: 'https://~',
-      sleepImage: 'https://~'
+      awakeImage: '/assets/skins/awakeOmokSkin0.png',
+      sleepImage: '/assets/skins/sleepOmokSkin0.png'
     },
     {
       rank: 2,
       memberId: 6,
       nickname: '오목이83838',
-      awakeImage: 'https://~',
-      sleepImage: 'https://~'
+      awakeImage: '/assets/skins/awakeOmokSkin3.png',
+      sleepImage: '/assets/skins/sleepOmokSkin3.png'
     }
   ]
 };
